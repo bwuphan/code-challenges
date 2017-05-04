af8a4f71fd270b93572d40921058b8e5d358cf0e
Bobby <bwuphan@gmail.com>
2017-05-04 16:33:15 -0700

    Solve validParantheses

--- Collection Two/validParantheses.js
+++ Collection Two/validParantheses.js
@@ -1,1 +1,27 @@
-const validParantheses = () => {}
+const validParantheses = (string) => {
+	const list = [];
+	const openings = {
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	};
+	const closings = {
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] in openings) {
+			list.push(string[i]);
+		} else if (string[i] in closings) {
+			if (list[list.length - 1] !== closings[string[i]]) {
+				return false;
+			} else {
+				list.pop();
+			}
+		}
+	}
+	return true;
+}
+
+console.log(validParantheses('[(])'))
