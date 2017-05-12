d094a098c59c01dd6a1a9789396ebd526088c1d9
bobby <bwuphan@gmail.com>
2017-05-11 22:47:57 -0700

    Solve shuffleDeck

--- Collection Three/shuffleDeck/shuffleDeck.js
+++ Collection Three/shuffleDeck/shuffleDeck.js
@@ -31,31 +31,6 @@
  *   See https://www.dartmouth.edu/~chance/teaching_aids/books_articles/Mann.pdf .
  */
 
-var shuffleDeck = function(deck) {
-  /* START SOLUTION */
-  // Constant space (in-place) solution
-  var swap = function (a, b) {
-    var temp = deck[a];
-    deck[a] = deck[b];
-    deck[b] = temp;
-  };
-  // we shuffle into the front of the array
-  // i is the first index of the un-shuffled portion
-  for (var i = 0; i < deck.length; i++) {
-    // select a random card from the un-shuffled portion
-    var pick = i + Math.floor(Math.random() * (deck.length - i));
-    // swap that card with the first un-shuffled card
-    swap(i, pick);
-    // since the pick is selected randomly each time, the swap is not biasing
-  }
-  return deck;
-  /* ELSE
-  // Your code here
-  END SOLUTION */
-};
-
-// Ordered deck generator provided for your testing convenience
-// (You may alter this function, but an unaltered copy will be used for tests.)
 var orderedDeck = function() {
   var suits = [ '♥', '♣', '♠', '♦' ];
   var values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
@@ -69,3 +44,63 @@ var orderedDeck = function() {
 
   return deck;
 };
+
+const swapCards = (idx1, idx2, array) => {
+  const tempVal = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = tempVal;
+};
+
+const shuffleDeck = (deck) => {
+  for (let i = 0; i < deck.length; i++) {
+    const index1 = Math.floor(Math.random() * 52);
+    const index2 = Math.floor(Math.random() * 52);
+    if (index1 !== index2) {
+      swapCards(index1, index2, deck);
+    };
+  };
+  return deck;
+};
+
+const test = orderedDeck();
+console.log(test);
+const shuffled = shuffleDeck(test);
+console.log(shuffled)
+// var shuffleDeck = function(deck) {
+//   /* START SOLUTION */
+//   // Constant space (in-place) solution
+//   var swap = function (a, b) {
+//     var temp = deck[a];
+//     deck[a] = deck[b];
+//     deck[b] = temp;
+//   };
+//   // we shuffle into the front of the array
+//   // i is the first index of the un-shuffled portion
+//   for (var i = 0; i < deck.length; i++) {
+//     // select a random card from the un-shuffled portion
+//     var pick = i + Math.floor(Math.random() * (deck.length - i));
+//     // swap that card with the first un-shuffled card
+//     swap(i, pick);
+//     // since the pick is selected randomly each time, the swap is not biasing
+//   }
+//   return deck;
+//   /* ELSE
+//   // Your code here
+//   END SOLUTION */
+// };
+
+// // Ordered deck generator provided for your testing convenience
+// // (You may alter this function, but an unaltered copy will be used for tests.)
+// var orderedDeck = function() {
+//   var suits = [ '♥', '♣', '♠', '♦' ];
+//   var values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
+//   var deck = [];
+
+//   suits.forEach(function(suit) {
+//     values.forEach(function(value) {
+//       deck.push(value + suit);
+//     });
+//   });
+
+//   return deck;
+// };
