69eb60546b2a1d86dd291fd7a1d254c8dc27b4e8
Bobby <bwuphan@gmail.com>
2017-05-11 17:47:20 -0700

    Solve bubbleSort

--- Collection Three/bubbleSort/bubbleSort.js
+++ Collection Three/bubbleSort/bubbleSort.js
@@ -31,44 +31,73 @@
 */
 
 // Introduce i into the global scope so we can test function efficiency
+
+
 var i;
 
 // Feel free to add helper functions if needed.
-/* START SOLUTION */
-var swap = function(index1, index2, array) {
-  var temp = array[index1];
-  array[index1] = array[index2];
-  array[index2] = temp;
+
+const swap = (idx1, idx2, array) => {
+  const tempVal = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = tempVal;
   return array;
 };
-/* END SOLUTION */
 
 var bubbleSort = function(array) {
   // Your code here.
-  /* START SOLUTION */
-  if (!Array.isArray(array)) {
-    throw new TypeError('bubbleSort accepts only arrays');
-  }
-
-  var len = array.length;
-
-  for (var i = 0; i < len; i++) {
-    var swaps = 0;
-
-    // Do j < len - 1 - i iterations so we don't consider the final (sorted)
-    // element in the array in future iterations
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (array[j] > array[j + 1]) {
-        swaps++;
-        swap(j, j + 1, array);
+  //create for loop iterate thru length of array starting at array.length - 1, then --
+  for (i = array.length - 1; i > 0; i--) {
+    //create nested for loop taht iterates until < i
+    for (let j = 1; j < i; j++) {
+      //check if next index is greater than than the current index, do swap
+      if (array[j] < array[j - 1]) {
+        array = swap(j, j - 1, array);
       }
     }
-
-    // If no swaps were done during this iteration, then the array is sorted
-    // and we can bail out
-    if (!swaps) { break; }
   }
-
   return array;
-  /* END SOLUTION */
 };
+
+console.log(bubbleSort([20, -10, -10.1, 2, 4, 299]))
+// var i;
+
+// // Feel free to add helper functions if needed.
+// /* START SOLUTION */
+// var swap = function(index1, index2, array) {
+//   var temp = array[index1];
+//   array[index1] = array[index2];
+//   array[index2] = temp;
+//   return array;
+// };
+// /* END SOLUTION */
+
+// var bubbleSort = function(array) {
+//   // Your code here.
+//   /* START SOLUTION */
+//   if (!Array.isArray(array)) {
+//     throw new TypeError('bubbleSort accepts only arrays');
+//   }
+
+//   var len = array.length;
+
+//   for (var i = 0; i < len; i++) {
+//     var swaps = 0;
+
+//     // Do j < len - 1 - i iterations so we don't consider the final (sorted)
+//     // element in the array in future iterations
+//     for (var j = 0; j < len - 1 - i; j++) {
+//       if (array[j] > array[j + 1]) {
+//         swaps++;
+//         swap(j, j + 1, array);
+//       }
+//     }
+
+//     // If no swaps were done during this iteration, then the array is sorted
+//     // and we can bail out
+//     if (!swaps) { break; }
+//   }
+
+//   return array;
+//   /* END SOLUTION */
+// };
