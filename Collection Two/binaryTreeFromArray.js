8cd9fcc37a035517e5d1f19be169bc3f18b9bcbb
Bobby <bwuphan@gmail.com>
2017-05-24 12:48:29 -0700

    Edit binaryTreeFromArray

--- Collection Two/binaryTreeFromArray.js
+++ Collection Two/binaryTreeFromArray.js
@@ -1,18 +1,18 @@
 const createBinaryTree = (array) => {
 	console.log(array)
 	if (array.length === 0) {
-		return;
+		return null;
 	}
 	const mid = array[Math.floor(array.length / 2)];
 	const leftArr = array.slice(0, Math.floor(array.length / 2));
 	const rightArr = array.slice(Math.floor(array.length / 2) + 1, array.length);
 	const node = {
 		value: mid,
-		left: leftArr.length > 0 ? createBinaryTree(leftArr) : null,
-		right: rightArr.length > 0 ? createBinaryTree(rightArr) : null,
+		left: createBinaryTree(leftArr),
+		right: createBinaryTree(rightArr)
 	}
 	return node;
 }
 
 
-console.log(createBinaryTree([1,2,3,4,5,6,7,8]));
+console.log(createBinaryTree([2,4,6,8,9]));
