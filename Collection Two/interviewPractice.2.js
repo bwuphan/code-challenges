d12060cf0aa33525a76bbc7bae29945c7b301767
Bobby <bwuphan@gmail.com>
2017-05-26 09:34:43 -0700

    Solve quickSort BFS and DFS in interviewPractice2

--- Collection Two/interviewPractice.2.js
+++ Collection Two/interviewPractice.2.js
@@ -64,3 +64,115 @@ graph.addNode(4);
 graph.addNode(5);
 graph.addEdge(4,2);
 graph.addEdge(5,3);
+
+Graph.prototype.depthFirstSearch = function(callback) {
+  const nodes = this.nodes;
+  const stack = [];
+  const visited = {};
+  const recurse = (node) => {
+    if (!node) {
+      return;
+    }
+    const edges = nodes[node].edges;
+    callback(node);
+    stack.push(node);
+    visited[node] = true;
+    for (let key in edges) {
+      if (!visited[key]) {
+        recurse(key);
+      }
+    }
+  };
+  const firstKey = Object.keys(nodes)[0];
+  recurse(firstKey);
+}
+
+graph.depthFirstSearch(function(node) {
+  console.log(node);
+});
+
+
+function Queue() {
+  this.storage = {};
+  let start = 0;
+  let end = 0;
+  this.enqueue = function(val) {
+    this.storage[end++] = val;
+  };
+  this.dequeue = function() {
+    if (this.size()) {
+      const dequeued = this.storage[start];
+      delete this.storage[start];
+      start++;
+      return dequeued;
+    }
+  };
+  this.size = function() {
+    return end - start;
+  }
+}
+
+
+
+Graph.prototype.breadthFirstSearch = function(fromNode, toNode) {
+  const nodes = this.nodes;
+  const queue = new Queue;
+  const visited = {};
+  const firstKey = Object.keys(nodes)[0];
+  queue.enqueue(firstKey);
+  visited[firstKey] = true;
+  while(queue.size() > 0) {
+    const dequeued = queue.dequeue();
+    console.log(dequeued)
+    const edges = nodes[dequeued].edges;
+    if (dequeued == fromNode) {
+      if (toNode in edges) {
+        return true;
+      }
+    }
+    if (dequeued == toNode) {
+      if (fromNode in edges) {
+        return true;
+      }
+    }
+    for (let key in edges) {
+      if (!visited[key]) {
+        queue.enqueue(key);
+        visited[key] = true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(graph.breadthFirstSearch(2,9));
+
+const quickSort = (array) => {
+  const partition = (start, end) => {
+    const pNum = array[end];
+    let pIndex = start;
+    for (let i = start; i < end; i++) {
+      if (array[i] < pNum) {
+        const temp = array[i];
+        array[i] = array[pIndex];
+        array[pIndex] = temp;
+        pIndex++;
+      }
+    }
+    const temp = array[end];
+    array[end] = array[pIndex];
+    array[pIndex] = temp;
+    return pIndex;
+  }
+  const recurse = (start, end) => {
+    if (start < end) {
+      const partitionIndex = partition(start, end);
+      recurse(start, partitionIndex - 1);
+      recurse(partitionIndex + 1, end);
+    }
+  };
+  recurse(0, array.length - 1);
+  return array;
+}
+
+console.log(quickSort([3,4,1,6,1,2,3,8,0,10]))
