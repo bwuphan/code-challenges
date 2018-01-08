cf11ccbc805c16fe54cba0ccecbff3caacbcc19d
Bobby <bwuphan@gmail.com>
2018-01-07 22:13:38 -0800

    Add removeK.js

---
+++ Collection Two/removeK.js
@@ -0,0 +1,22 @@
+// Task
+// Given a number k, find the smallest integer n such that if any k elements are removed from the set {1, 2, ..., n}, one can still find k distinct numbers among the remaining elements with sum of n.
+
+// Input/Output
+// [input] integer k
+
+// The number of elements to be removed.
+
+// 2 ≤ k ≤ 10000
+
+// [output] an integer
+
+// The smallest value of n.
+
+// Example
+// For k = 2, the output should be 7.
+
+// The initial set is {1, 2, 3, 4, 5, 6, 7}.
+
+// No matter what 2 elements you remove, (1, 6), (2, 5), or (3, 4) will sum up to 7.
+
+// Why the result isn't 6? Because:
