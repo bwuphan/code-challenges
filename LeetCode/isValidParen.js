898ff1dc59e40ed51c40a830b11df906e014979c
Bobby Phan <bwuphan@gmail.com>
2019-07-22 21:46:06 -0700

    Add solution for isValidParen.js

--- LeetCode/isValidParen.js
+++ LeetCode/isValidParen.js
@@ -4,10 +4,20 @@ const Stack = require('../Prototypes/Stack').Stack;
 
 var test = new Stack();
 
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
+  const left  = { '(' : ')', '[' : ']', '{' : '}' };
+  const right = { ')' : '(', ']' : '[', '}' : '{' };
 
+  let pStack = new Stack();
+  for (let i = 0; i < s.length; ++i) {
+    const cur = s[i];
+    if (cur in left) pStack.push(cur);
+    else if (cur in right) {
+      const popped = pStack.pop();
+      if (popped !== right[cur]) {
+        return false;
+      }
+    }
+  }
+  return pStack._storage.length === 0;
 };
