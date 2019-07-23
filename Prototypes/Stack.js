3af968aba1c7cfc48a758310fbf88284bf3d0fe1
Bobby Phan <bwuphan@gmail.com>
2019-07-22 21:24:09 -0700

    Add Prototypes/Stack.js

---
+++ Prototypes/Stack.js
@@ -0,0 +1,11 @@
+var Stack = function() {
+  this._storage = [];
+}
+
+Stack.prototype.push = function(val) {
+  return this._storage.push(val);
+}
+
+Stack.prototype.pop = function() {
+  return this._storage.pop();
+}
