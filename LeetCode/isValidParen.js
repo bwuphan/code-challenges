bebcf1b7900f02e02d8913c17838b7d975bcd9d0
Bobby Phan <bwuphan@gmail.com>
2019-07-22 21:32:33 -0700

    Add isValidParen.js

---
+++ LeetCode/isValidParen.js
@@ -0,0 +1,13 @@
+// import { Stack } from '../Prototypes/Stack.js'
+
+const Stack = require('../Prototypes/Stack').Stack;
+
+var test = new Stack();
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+};
