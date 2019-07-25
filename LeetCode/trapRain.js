6706a2e5780a9eeb2762275d15b626348888c692
Bobby Phan <bwuphan@gmail.com>
2019-07-24 22:07:17 -0700

    Add dynamic programming solution for trapRain.js

--- LeetCode/trapRain.js
+++ LeetCode/trapRain.js
@@ -3,29 +3,61 @@
  * @return {number}
  */
 
-const trap = function(height) {
-  const calcSingleCol = (index, height) => {
-    let hLeft = null;
-    for (let i = index; i >= 0; --i) {
-      if (hLeft === null || height[i] > hLeft) hLeft = height[i];
-    }
+// Dynamic Programming solution
 
-    let hRight = null;
-    for (let i = index; i < height.length; ++i) {
-      if (hRight === null || height[i] > hRight) hRight = height[i];
-    }
-    const returnVal = Math.min(hLeft, hRight) - height[index];
-    return returnVal >= 0 ? returnVal : 0;
+const trap = function(height) {
+
+  let maxLeft = 0, maxRight = 0;
+  let maxLeftObj = { '0' : 0 };
+  let maxRightObj = {}; maxRightObj[height.length - 1] = 0;
+
+  for (let i = 1; i < height.length; ++i) {
+    if (height[i - 1] > maxLeft) maxLeft = height[i - 1];
+    maxLeftObj[i] = maxLeft;
   }
+
+  for (let i = height.length - 2; i >= 0; --i) {
+    if (height[i + 1] > maxRight) maxRight = height[i + 1];
+    maxRightObj[i] = maxRight;
+  }
+
 
   let heightSum = 0;
   for (let i = 0; i < height.length; ++i) {
-    const hLeft = (i - 1) >= 0 ? height[i - 1] : 0;
-    const hRight = (i + 1) < height.length ? height[i + 1] : 0;
-    const hCol = height[i];
+    const hCol = Math.min(maxLeftObj[i], maxRightObj[i]) - height[i];
 
-    heightSum += calcSingleCol(i, height);
+    heightSum += hCol >= 0 ? hCol : 0;
   }
 
   return heightSum;
 };
+
+
+// Brute Force
+
+// const trap = function(height) {
+//   const calcSingleCol = (index, height) => {
+//     let hLeft = null;
+//     for (let i = index; i >= 0; --i) {
+//       if (hLeft === null || height[i] > hLeft) hLeft = height[i];
+//     }
+
+//     let hRight = null;
+//     for (let i = index; i < height.length; ++i) {
+//       if (hRight === null || height[i] > hRight) hRight = height[i];
+//     }
+//     const returnVal = Math.min(hLeft, hRight) - height[index];
+//     return returnVal >= 0 ? returnVal : 0;
+//   }
+
+//   let heightSum = 0;
+//   for (let i = 0; i < height.length; ++i) {
+//     const hLeft = (i - 1) >= 0 ? height[i - 1] : 0;
+//     const hRight = (i + 1) < height.length ? height[i + 1] : 0;
+//     const hCol = height[i];
+
+//     heightSum += calcSingleCol(i, height);
+//   }
+
+//   return heightSum;
+// };
