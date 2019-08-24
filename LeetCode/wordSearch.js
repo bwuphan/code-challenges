81817c3c7a6da10093a90c7dc1f66e60e916676f
bwuphan <bwuphan@gmail.com>
2019-08-24 14:35:40 -0700

    Add solution for wordSearch.js

--- LeetCode/wordSearch.js
+++ LeetCode/wordSearch.js
@@ -26,5 +26,49 @@ Given word = "ABCB", return false.
  * @return {boolean}
  */
 var exist = function(board, word) {
+  let exists = false;
+  const traverse = function(board, idx, y, x) {
+    if (y < 0 || y >= board.length || x < 0 || x >= board[0].length || board[y][x] === null || exists === true) {
+      return;
+    }
+
+    if (board[y][x] === word[idx]) {
+      if (idx === word.length - 1) {
+        exists = true;
+        return;
+      }
+      board = JSON.parse(JSON.stringify(board));
+      board[y][x] = null;
+      idx++;
+
+      traverse(board, idx, y + 1, x);
+      traverse(board, idx, y - 1, x);
+      traverse(board, idx, y, x + 1);
+      traverse(board, idx, y, x - 1);
+
+    }
+  }
+
+  for (let i = 0; i < board.length; ++i) {
+    for (let j = 0; j < board[0].length; ++j) {
+      const curLetter = board[i][j];
+      if (curLetter === word[0]) {
+        traverse(board, 0, i, j);
+      }
+    }
+  }
+
+  return exists;
+
 
 };
+
+var board = [
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+];
+
+console.log(exist(board, 'ABCCED'));
+console.log(exist(board, 'SEE'));
+console.log(exist(board, 'ABCB'));
