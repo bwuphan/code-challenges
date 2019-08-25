4e7b1e68a75d3212bcff3b5f3bbc553096c243cb
bwuphan <bwuphan@gmail.com>
2019-08-24 23:38:47 -0700

    Add binaryTreeZigzagTraversal.js

---
+++ LeetCode/binaryTreeZigzagTraversal.js
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+*/
+const Queue = require('../Prototypes/Queue').Queue;
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+
+};
+
