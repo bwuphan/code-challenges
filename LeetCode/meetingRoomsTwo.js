2dc48aad16092332a1cd6b2f8a46cb84b16f467c
bwuphan <bwuphan@gmail.com>
2019-08-25 21:38:12 -0700

    Add solution for meetingRoomsTwo.js

--- LeetCode/meetingRoomsTwo.js
+++ LeetCode/meetingRoomsTwo.js
@@ -21,7 +21,39 @@ definition to get new method signature.
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
+  // If intervals are empty, just return 0.
+  if (intervals.length === 0) return 0;
 
+  // Sort intervals by meeting start time ascending.
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+
+  // Initialize the first meeting room.
+  let meetingRooms = [[intervals[0]]];
+
+  for (let i = 1; i < intervals.length; ++i) {
+    const curInterval = intervals[i];
+
+    // Tells if the interval has been pushed to an existing meeting room.
+    let intervalPushed = false;
+
+    for (let j = 0; j < meetingRooms.length; ++j) {
+      const curMeetingRoom = meetingRooms[j];
+
+      // If you find an open meeting room, push to that meeting room and set intervalPushed to true.
+      if (curInterval[0] >= curMeetingRoom[curMeetingRoom.length - 1][1]) {
+        intervalPushed = true;
+        curMeetingRoom.push(curInterval);
+        break;
+      }
+    }
+
+    // If the interval has yet to be pushed to a meeting room, create a new meeting room.
+    if (!intervalPushed) {
+      meetingRooms.push([curInterval]);
+    }
+  }
+
+  return meetingRooms.length;
 };
 
 console.log(minMeetingRooms([[0,30],[5,10],[15,20]]));
