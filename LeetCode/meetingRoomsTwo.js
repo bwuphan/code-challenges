c3f9ab2b556476c6938eba932dfb533598389ea0
bwuphan <bwuphan@gmail.com>
2019-08-25 21:08:09 -0700

    Add prompt for meetingRoomsTwo.js

---
+++ LeetCode/meetingRoomsTwo.js
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/meeting-rooms-ii/
+
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+NOTE: input types have been changed on April 15, 2019. Please reset to default code
+definition to get new method signature.
+*/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+
+};
+
+console.log(minMeetingRooms([[0,30],[5,10],[15,20]]));
+console.log(minMeetingRooms([[7,10],[2,4]]));
