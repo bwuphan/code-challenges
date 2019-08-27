6b5d40e6dc19d7ef206dfe3cca06d1f70d19ad0c
bwuphan <bwuphan@gmail.com>
2019-08-27 00:58:02 -0700

    Add solution to medianTwoSortedArr.js

--- LeetCode/medianTwoSortedArr.js
+++ LeetCode/medianTwoSortedArr.js
@@ -27,5 +27,44 @@ The median is (2 + 3)/2 = 2.5
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+  const totLength = nums1.length + nums2.length;
+  const isEven = totLength % 2 === 0;
 
+  let counter1 = 0;
+  let counter2 = 0;
+
+  // Index to stop the combination of the arrays (inclusive).
+  const stopIdx = Math.floor(totLength / 2);
+
+  let i = 0;
+
+  // The combined array of nums1 and nums2.
+  let combinedArr = [];
+
+  /* We are going to add numbers from nums1 and nums2 to the combined array in order. Once it
+     gets to the stopIdx, we will stop. */
+  while(i <= stopIdx) {
+    if ((typeof nums1[counter1] !== 'undefined' && nums1[counter1] <= nums2[counter2]) || typeof nums2[counter2] === 'undefined') {
+      combinedArr.push(nums1[counter1++]);
+    }
+    else {
+      combinedArr.push(nums2[counter2++]);
+    }
+
+    i++;
+  }
+
+  if (isEven) {
+    return (combinedArr[combinedArr.length - 1] + combinedArr[combinedArr.length - 2]) / 2;
+  }
+  else {
+    return combinedArr[combinedArr.length - 1];
+  }
 };
+
+
+console.log(findMedianSortedArrays([1,3],[2]));
+console.log(findMedianSortedArrays([1,2],[3,4]));
+console.log(findMedianSortedArrays([],[1]));
+console.log(findMedianSortedArrays([2],[]));
+console.log(findMedianSortedArrays([0,0],[0,0]));
