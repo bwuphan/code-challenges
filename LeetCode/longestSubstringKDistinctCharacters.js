5cc1fdcd01d25fdeb52f3a553b579e3d2f65657b
bwuphan <bwuphan@gmail.com>
2019-08-27 20:56:59 -0700

    Add LeetCode/longestSubstringKDistinctCharacters.js

---
+++ LeetCode/longestSubstringKDistinctCharacters.js
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: T is "ece" which its length is 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: T is "aa" which its length is 2.
+*/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+
+};
