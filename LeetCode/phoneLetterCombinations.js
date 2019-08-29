98c39c61f9366acb09a75b662ed13b5a59183a69
bwuphan <bwuphan@gmail.com>
2019-08-29 08:44:30 -0700

    Fix typo in LeetCode/phoneLetterCombinations.js

--- LeetCode/phoneLetterCombinations.js
+++ LeetCode/phoneLetterCombinations.js
@@ -25,7 +25,7 @@ var letterCombinations = function(digits) {
   let phoneMap = {
     '2': ['a','b','c'],
     '3': ['d','e','f'],
-    '4': ['g','g','i'],
+    '4': ['g','h','i'],
     '5': ['j','k','l'],
     '6': ['m','n','o'],
     '7': ['p','q','r','s'],
