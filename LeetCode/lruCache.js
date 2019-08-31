92fc2ae766c03b39614ee70d135b1a4277c93391
bwuphan <bwuphan@gmail.com>
2019-08-31 13:45:00 -0700

    WIP lruCache.js

--- LeetCode/lruCache.js
+++ LeetCode/lruCache.js
@@ -1,86 +1,134 @@
-/**
- * @param {number} capacity
- */
-var Node = function(data) {
-  this.data = data;
-  this.prev = null;
+var Node = function(key, val) {
+  this.key = key;
+  this.val = val;
   this.next = null;
+  this.prev = null;
 }
 
-var dLinkedList = function() {
+
+var LinkedList = function() {
   this.head = null;
   this.tail = null;
-  this.numNodes = 0;
-
 }
-dLinkedList.prototype.addToTail = function(data) {
-  const newNode = new Node(data);
-  if (!this.head && !this.tail) {
+
+LinkedList.prototype.addToTail = function(key, val) {
+  const newNode = new Node(key, val);
+  // If there is no head, make this the new head.
+  if (!this.head) {
     this.head = newNode;
-  } else {
+  }
+  // Else, add to tail.
+  else {
+    const oldTail = this.tail;
+    // If there is no tail, add tail to head.
     if (!this.tail) {
       this.head.next = newNode;
       newNode.prev = this.head;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
     }
+    // Else, add to previous tail.
+    else {
+      newNode.prev = oldTail;
+      oldTail.next = newNode;
+    }
+
+    // Set tail to newest node.
     this.tail = newNode;
   }
-  this.numNodes++;
+
   return newNode;
 }
 
-dLinkedList.prototype.removeFromHead = function() {
-  // console.log('in remove from head');
-  if (!this.head) return null;
-  else {
-    const tempNode = this.head;
-    if (!this.tail) {
-      this.head = null;
-    } else {
-      this.head = this.head.next;
-      this.head.prev = null;
+LinkedList.prototype.removeFromHead = function() {
+  // Only remove if a head exists.
+  if (this.head) {
+    const curHead = this.head;
+    this.head = this.head.next;
+
+    // If the head and tail are the same, this is the only node and we need to null out this.tail.
+    if (this.head === this.tail) {
+      this.tail = null;
     }
-    this.numNodes--;
-    // console.log('DLL');
-    // printDll(this);
-    return tempNode.data;
+
+    return curHead;
+  }
+  return null;
+}
+
+LinkedList.prototype.removeFromTail = function() {
+  // Only remove if a tail exists.
+  if (this.tail) {
+    this.tail = this.tail.prev;
+    this.tail.next = null;
   }
 }
 
-dLinkedList.prototype.removeNode = function(Node) {
-  // console.log('NODE TO REMOVE', Node);
-  const prevNode = Node.prev;
-  const nextNode = Node.next;
-  if (!Node.prev && !Node.next) {
-    this.head = null;
-  } else if (!Node.prev) {
-    nextNode.prev = null;
-    // console.log('new HEEEEEEEEEEEAAAADDDDDSB', nextNode);
-    this.head = nextNode;
-  } else if (!Node.next) {
-    prevNode.next = null;
-    if (this.tail) this.tail = prevNode;
-  } else {
-    prevNode.next = nextNode;
-    nextNode.prev = prevNode;
+LinkedList.prototype.removeNode = function(node) {
+  if (this.head === node) {
+    this.removeFromHead();
   }
-  this.numNodes--;
+  else if (this.tail === node) {
+    this.removeFromTail();
+  }
+  else {
+    // Connect previous node to next node.
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
 }
 
+LinkedList.prototype.moveToTail = function(node) {
+  this.removeNode(node);
+  return this.addToTail(node.val);
+}
+
+LinkedList.prototype.removeDuplicates = function() {
+  let valObj = {};
+
+  let curNode = this.head;
+  while(curNode !== null) {
+    const nextNode = curNode.next;
+    // If the current node value is in valObj, remove the node.
+    if (curNode.val in valObj) {
+      this.removeNode(curNode);
+    }
+    // Else, set current node value in valObj.
+    else {
+      valObj[curNode.val] = true;
+    }
+
+    curNode = nextNode;
+  }
+}
+
+LinkedList.prototype.outputVals = function() {
+  let curNode = this.head;
+  while (curNode !== null) {
+    console.log(curNode.val);
+    curNode = curNode.next;
+  }
+}
+
+LinkedList.prototype.returnKthFromTail = function(k) {
+  let i = 0;
+  let curNode = this.tail;
+  while (curNode !== null) {
+    if (i === k) {
+      return curNode;
+    }
+    i++;
+    curNode = curNode.prev;
+  }
+  return null;
+}
+
+/**
+ * @param {number} capacity
+ */
 var LRUCache = function(capacity) {
-  this.dll = new dLinkedList();
-  this.capacity = capacity;
-  this.object = {};
-};
-
-var LRUCacheItem = function (key, value) {
-  /* START SOLUTION */
-  this.value = value === undefined ? null : value;
-  this.key = key === undefined ? null : key;
-  this.node = null;
-  /* END SOLUTION */
+  this._capacity = capacity;
+  this._size = 0;
+  this._cache = new LinkedList();
+  this._map = new Map();
 };
 
 /**
@@ -88,28 +136,18 @@ var LRUCacheItem = function (key, value) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-  console.log('GEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTTTT', key);
-  console.log(this.object);
-  printDll(this.dll);
-  // printDll(this.dll);
-  let returnVal = -1;
-  if (key in this.object) {
-    if (this.capacity >= this.dll.numNodes) {
-      this.dll.removeNode(this.object[key]);
-      // console.log('AFTER REMOVAL'); printDll(this.dll);
-      const newItem = new LRUCacheItem(key, this.object[key].data);
-      newItem.node = this.dll.addToTail(this.object[key].data);
-      this.object[key] = newItem;
-      console.log('new item in get', newItem);
-    }
-    returnVal = this.object[key].value;
+  // console.log(this._map);
+  if (key in this._map) {
+    this._cache.moveToTail(this._map.get(key));
   }
-  // console.log('END OF GET'); printDll(this.dll);
-  // console.log(returnVal);
-  console.log('Final Obj', this.object);
-  console.log('rval', returnVal);
-  return returnVal;
-
+  // console.log(this._map.get(key));
+  if (this._map.get(key)) {
+    return this._map.get(key).val;
+  }
+  else {
+    return null;
+  }
+  // return this._map.get(key).val;
 };
 
 /**
@@ -118,21 +156,26 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-  console.log('PUT', key, value);
-  if (key in this.object) {
-    // console.log('DUP')
-    this.dll.removeNode(this.object[key]);
+  // If we are already at capacity.
+  if (this._size === this._capacity) {
+    const removedNode = this._cache.removeFromHead();
+    this._map.delete(removedNode.key);
+    this._size--;
+    this.put(key, value);
   }
-  if (this.dll.numNodes >= this.capacity) {
-    const headVal = this.dll.removeFromHead();
-    delete this.object[key];
+  else {
+    // If the key already exists in the cache, just get.
+    if (key in this._map) {
+      this.get(key);
+    }
+    // Else, set values as usual.
+    else {
+      const newNode = this._cache.addToTail(key, value);
+      this._map.set(key, newNode);
+      this._size++;
+    }
   }
-  const newItem = new LRUCacheItem(key, value);
-  newItem.node = this.dll.addToTail(value);
-
-  this.object[key] = newItem;
-  console.log('Final Obj', this.object);
-  // console.log(printDll(this.dll));
+  console.log(this._map.keys());
 };
 
 /**
@@ -141,79 +184,3 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
-
- let dll = new dLinkedList();
- dll.addToTail(5);
- dll.addToTail(4);
- dll.addToTail(3);
-
-// printDll(dll);
-// console.log(dll.removeFromHead());
-// printDll(dll);
-
-function printDll(dll) {
-  console.log(`Head: ${dll.head ? dll.head.data : null}`);
-  console.log(`Tail: ${dll.tail ? dll.tail.data : null}`);
-  console.log(`Num Nodes: ${dll.numNodes}`);
-  printNode(dll.head);
-}
-function printNode(Node) {
-  console.log(`Val here: ${Node.data} `);
-  if (!Node.next) return;
-  else {
-    return printNode(Node.next);
-  }
-}
-
-// let lru = new LRUCache(3);
-// lru.put(1,1);
-// lru.put(2,2);
-// lru.put(3,3);
-// lru.put(4,4);
-// lru.get(4);
-// lru.get(3);
-// lru.get(2);
-// // console.log(lru.object);
-// lru.get(1);
-// lru.put(5,5);
-// lru.get(1);
-// // printDll(lru.dll);
-// // console.log(lru.object);
-// lru.get(2);
-// lru.get(3);
-// lru.get(4);
-// lru.get(5);
-
-
-/*
-["LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"]
-[[3],        [1,1],[2,2],[3,3],[4,4],[4],  [3],  [2],  [1],  [5,5],[1],  [2],  [3],  [4],  [5]]
-*/
-
-// let lru = new LRUCache(1);
-// lru.put(2,1);
-// lru.get(2);
-// console.log(lru.object);
-// printDll(lru.dll);
-// lru.put(3,2);
-// console.log("HEW");
-// console.log(lru.object);
-// printDll(lru.dll);
-// lru.get(2);
-// lru.get(3)
-
-let lru = new LRUCache(2)
-lru.put(1,1);
-lru.put(2,2);
-lru.get(1);
-// console.log(lru.object);
-// printDll(lru.dll);
-lru.put(3,3);
-// console.log("HEW");
-// console.log(lru.object);
-// printDll(lru.dll);
-lru.get(2);
-lru.put(4,4);
-lru.get(3)
-lru.get(1)
-lru.get(4)
