1742fdcdc4b5b93864032d0715b03c12b7faaffe
bwuphan <bwuphan@gmail.com>
2019-09-11 20:45:43 -0700

    Add solution to LeetCode/courseSchedule.js

--- LeetCode/courseSchedule.js
+++ LeetCode/courseSchedule.js
@@ -33,5 +33,65 @@ You may assume that there are no duplicate edges in the input prerequisites.
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
+  if (prerequisites.length === 0) return true;
 
+  // Create graph of prerequisites.
+  let graph = {};
+  prerequisites.forEach(prereq => {
+    const val = prereq.shift();
+    if (val in graph) {
+      graph[val] = graph[val].concat(prereq);
+    }
+    else {
+      graph[val] = prereq;
+    }
+  });
+
+  // The unvisited nodes.
+  let unvisited = { ...graph };
+
+  /* The nodes that are currently being visited. If we meet a node in here while we are traversing
+     this node, there is a cycle */
+  let visiting = {};
+
+  // These are the nodes that are done being visited.
+  let done = {};
+
+  let hasCycle = false;
+  const dfs = function(courseNum) {
+    if (courseNum in visiting) {
+      hasCycle = true;
+      return;
+    }
+
+    if (!hasCycle) {
+      visiting[courseNum] = true;
+
+      delete unvisited[courseNum];
+
+      if (graph[courseNum] !== undefined) {
+        graph[courseNum].forEach(num => {
+          dfs(num);
+        });
+      }
+      delete visiting[courseNum];
+
+      done[courseNum] = true;
+    }
+  }
+
+  for (let key in graph) {
+    dfs(key);
+
+    if (hasCycle) {
+      return false;
+    }
+  }
+  return true;
 };
+
+
+// console.log(canFinish(2, [[1,0]]));
+// console.log(canFinish(2, [[1,0],[0,1]]));
+// console.log(canFinish(3, [[1,0],[1,2],[0,1]]));
+console.log(canFinish(4, [[2,0],[1,0],[3,1],[3,2],[1,3]]));
