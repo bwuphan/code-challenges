f3dd7635533997d3e4c204ccc6f2503c58134577
bwuphan <bwuphan@gmail.com>
2019-09-17 20:39:50 -0700

    Add solution for LeetCode/jumpGame.js

--- LeetCode/jumpGame.js
+++ LeetCode/jumpGame.js
@@ -26,9 +26,70 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
  * @return {boolean}
  */
 var canJump = function(nums) {
+  let resultBool = false;
 
+  /* Create memo where elements are null if it hasn't been traveled, false if there is no solution
+     at that particular index, and true if there is a solution. */
+  let memo = {};
+  nums.forEach((num, i) => {
+    if (i === nums.length - 1) {
+      memo[i] = true;
+    }
+    else {
+      memo[i] = null;
+    }
+  });
+
+  // The jump function.
+  const jump = function(idx) {
+    // If we are at the final index, then we found a solution.
+    if (idx === nums.length - 1) {
+      resultBool = true;
+      memo[idx] = true;
+      return true;
+    }
+    // If we have traveled this idx and it is false, then return false.
+    if (memo[idx] === false) {
+      return false;
+    }
+
+    // If we haven't yet found a solution and this idx has not yet been traveled, proceed.
+    if (!resultBool && memo[idx] === null) {
+      // Loop through possible jumps for this index.
+      for (let i = 1; i <= nums[idx]; ++i) {
+        const jumpIdx = idx + i;
+        // If the index to be stepped in has yet to be traveled, call jump.
+        if (memo[jumpIdx] === null) {
+          const thisJump = jump(jumpIdx);
+          // If this particular jump returned false, set memo for this index to be false;
+          if (!thisJump) {
+            memo[jumpIdx] = false;
+          }
+          // Else if this particular jump returned a solution, set memo and resultBool to true.
+          else if (thisJump) {
+            memo[jumpIdx] = true;
+            resultBool = true;
+            return true;
+          }
+        }
+        // Else if this memo has already been traveled, set resultBool to true and return.
+        else if (memo[jumpIdx] === true) {
+          resultBool = true;
+          return true;
+        }
+      }
+      // If we reached this point without a return, then we know there is no solution for this idx.
+      return false;
+    }
+  }
+
+  jump(0);
+
+  return resultBool;
 };
 
 
-console.log(canJump([2,3,1,1,4]));
-console.log(canJump([3,2,1,0,4]))
+// console.log(canJump([2,3,1,1,4]));
+// console.log(canJump([3,2,1,0,4]));
+// console.log(canJump([1,2]));
+console.log(canJump([2,0,0]));
