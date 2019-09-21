a4201b40f67da091149d86e557c0c7227b867415
bwuphan <bwuphan@gmail.com>
2019-09-21 14:57:22 -0700

    Add binaryTreeRightSideView.js solution

--- LeetCode/binaryTreeRightSideView.js
+++ LeetCode/binaryTreeRightSideView.js
@@ -1,5 +1,6 @@
 /*
 https://leetcode.com/problems/binary-tree-right-side-view/
+
 Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
 Example:
@@ -14,3 +15,61 @@ Explanation:
  \     \
   5     4       <---
 */
+
+var Queue = function() {
+  this.storage = [];
+}
+
+Queue.prototype.enqueue = function(item) {
+  this.storage.push(item);
+  return item;
+}
+
+Queue.prototype.dequeue = function() {
+  return this.storage.shift();
+}
+
+Queue.prototype.isEmpty = function() {
+  return this.storage.length === 0;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  if (!root) return [];
+  
+  let queue = new Queue();
+  queue.enqueue({ node: root, depth: 0 });
+  
+  let results = [];
+  let curDepth = 0;
+  while (!queue.isEmpty()) {
+    let item = queue.dequeue();
+
+    // Check we found an iten that is the first of the curDepth.
+    if (curDepth === item.depth) {
+    	// Push to results and increase curDepth.
+      results.push(item.node.val);
+      curDepth++;
+    }
+
+    // Go right first since we're reading from right to left.
+    if (item.node.right) {
+      queue.enqueue({ node: item.node.right, depth: item.depth + 1});
+    }
+    if (item.node.left) {
+      queue.enqueue({ node: item.node.left, depth: item.depth + 1 });
+    }
+  }
+  
+  return results;
+};
