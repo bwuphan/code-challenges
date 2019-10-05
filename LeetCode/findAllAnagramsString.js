addd655ecd1cb8d22ccb22fe94b7d03bc7d64036
bwuphan <bwuphan@gmail.com>
2019-10-05 16:37:42 -0700

    Add solution to LeetCode/findAllAnagramsString.js

--- LeetCode/findAllAnagramsString.js
+++ LeetCode/findAllAnagramsString.js
@@ -39,5 +39,61 @@ The substring with start index = 2 is "ab", which is an anagram of "ab".
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
+  const pMap = new Map(); // Create map for solution or p substring.
+  let slidingWindow = new Map(); // Create initial sliding window.
+  for (let i = 0; i < p.length; ++i) {
+    const curLetterP = p[i];
+    if (pMap.has(curLetterP)) pMap.set(curLetterP, pMap.get(curLetterP) + 1);
+    else pMap.set(curLetterP, 1);
 
+    const curLetterS = s[i];
+    if (slidingWindow.has(curLetterS)) slidingWindow.set(curLetterS, slidingWindow.get(curLetterS) + 1);
+    else slidingWindow.set(curLetterS, 1);
+  }
+
+  let results = []; // Results array to store indices.
+
+  // If pMap matches init sliding window, 0 is a solution.
+  if (compareMaps(pMap, slidingWindow)) results.push(0);
+
+  // Start at index 1 and go until the end of the s string but minus the length of the p string.
+  for (let i = 1; i < (s.length - p.length + 1); ++i) {
+    const prevLetter = s[i - 1];
+    const curLastLetter = s[i + p.length - 1];
+
+    // Remove the previous letter from the sliding window.
+    slidingWindow.set(prevLetter, slidingWindow.get(prevLetter) - 1);
+    if (slidingWindow.get(prevLetter) === 0) {
+      slidingWindow.delete(prevLetter);
+    }
+
+    // Add the current new last letter to the sliding window.
+    const numCurLastLetter = slidingWindow.get(curLastLetter) || 0;
+    slidingWindow.set(curLastLetter, numCurLastLetter + 1);
+
+    // If the pMap and sliding window are the same, we have a solution.
+    if (compareMaps(pMap, slidingWindow)) {
+      results.push(i);
+    }
+  }
+  return results;
 };
+
+// Compare two maps. Return true if they match.
+function compareMaps(map1, map2) {
+  if (map1.size !== map2.size) return false;
+
+  let match = true;
+
+  map1.forEach((value, key) => {
+    if (value !== map2.get(key)) {
+      match = false;
+    }
+  });
+
+  return match;
+}
+
+console.log(findAnagrams("cbaebabacd", "abc"))
+console.log(findAnagrams("abab", "ab"))
+console.log(findAnagrams("", "a"))
