74c5481ee2bc4f2a29aa504dfd6eb95e4918d372
bwuphan <bwuphan@gmail.com>
2019-10-06 23:58:22 -0700

    Add solution to LeetCode/validPalindrome2.js

--- LeetCode/validPalindrome2.js
+++ LeetCode/validPalindrome2.js
@@ -15,14 +15,66 @@ Note:
 The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
 */
 
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  console.log(s);
+  let sArr = s.toLowerCase().split('').filter(char => char.match(/^[0-9a-z]+$/));
+
+  let left, right = null;
+
+  if (sArr.length % 2 === 0) {
+    left = sArr.length / 2 - 1;
+    right = left + 1;
+  }
+  else {
+    left = Math.floor(sArr.length / 2) - 1;
+    right = left + 2;
+  }
+
+  // If left is less than 0, we are done.
+  while (left >= 0) {
+    if (sArr[left] !== sArr[right]) {
+      return false;
+    }
+
+    left--;
+    right++;
+  }
+
+  return true;
+};
+
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 var validPalindrome = function(s) {
+  let sArr = s.toLowerCase().split('').filter(char => char.match(/^[0-9a-z]+$/));
 
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    if (sArr[left] !== sArr[right]) {
+      let unshift = sArr.slice(left, right + 1);
+      unshift.unshift();
+      let pop = sArr.slice(left, right + 1);
+      pop.pop();
+      console.log(unshift, pop);
+      return (isPalindrome(unshift.join('')) || isPalindrome(pop.join('')));
+    }
+
+    left++;
+    right--;
+  }
+  return true;
 };
 
 
 console.log(validPalindrome('aba'));
-console.log(validPalindrome('abca'));
+console.log(validPalindrome('abbbaca'));
