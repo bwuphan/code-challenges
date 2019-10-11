c8c63e5672ca39c15e488247007301d0c5aa0c26
bwuphan <bwuphan@gmail.com>
2019-10-10 22:17:04 -0700

    Add solution for LeetCode/diameterOfBinaryTree.js

--- LeetCode/diameterOfBinaryTree.js
+++ LeetCode/diameterOfBinaryTree.js
@@ -29,6 +29,33 @@ Note: The length of path between two nodes is represented by the number of edges
  * @param {TreeNode} root
  * @return {number}
  */
-var diameterOfBinaryTree = function(root) {
 
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+  if (!root || (!root.left && !root.right)) return 0;
+  let max = 0;
+
+  const dfs = function(node) {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+    max = Math.max(max, left + right);
+    return Math.max(left, right) + 1;
+  }
+
+  dfs(root);
+
+  return max;
 };
