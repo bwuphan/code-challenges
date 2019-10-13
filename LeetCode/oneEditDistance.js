c22440c97b0af7ab341e3d5cf6add7003658e5e6
bwuphan <bwuphan@gmail.com>
2019-10-12 20:50:36 -0700

    Add comments to LeetCode/oneEditDistance.js

--- LeetCode/oneEditDistance.js
+++ LeetCode/oneEditDistance.js
@@ -33,5 +33,32 @@ Explanation: We can replace '0' with '1' to get t.
  * @return {boolean}
  */
 var isOneEditDistance = function(s, t) {
+  if (s === t) return false;
 
+  const diffLength = s.length - t.length;
+
+  if (diffLength > 1 || diffLength < -1) return false;
+
+
+  for (let i = 0; i < Math.max(s.length, t.length); ++i) {
+    if (s[i] !== t[i]) {
+      if (diffLength === 0) {
+        s = s.slice(0, i) + t[i] + s.slice(i + 1, s.length);
+      }
+      else if (diffLength === 1) {
+        s = s.slice(0, i) + s.slice(i + 1, s.length);
+      }
+      else if (diffLength === -1) {
+        s = s.slice(0, i) + t[i] + s.slice(i, s.length);
+      }
+
+      // If strings are equal, then we are good, else it is not possible.
+      if (s === t) return true;
+      else return false;
+    }
+  }
 };
+
+console.log(isOneEditDistance(s = "ab", t = "acb"));
+console.log(isOneEditDistance(s = "cab", t = "ad"));
+console.log(isOneEditDistance(s = "1203", t = "1213"));
