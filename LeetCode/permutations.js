8ce55ce97727d5c087dd776a02f47e9297fb9651
bwuphan <bwuphan@gmail.com>
2019-10-14 21:51:34 -0700

    Add LeetCode/permutations.js

---
+++ LeetCode/permutations.js
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/permutations/
+
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+
+};
+
+
+console.log(permute([1,2,3]));
