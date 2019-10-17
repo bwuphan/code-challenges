a4263600b62ab8d3baf27e230b9eb7587050d479
bwuphan <bwuphan@gmail.com>
2019-10-16 20:06:41 -0700

    Add LeetCode/search2dMatrix.js

---
+++ LeetCode/search2dMatrix.js
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the
+following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+Accepted
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+
+};
+
+const matrix = [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+];
+
+console.log(searchMatrix(matrix, 5));
+
+console.log(searchMatrix(matrix, 20));
