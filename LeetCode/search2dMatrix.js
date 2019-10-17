d3a3c3f3378629619c6cc1bb7229e91ae146c788
bwuphan <bwuphan@gmail.com>
2019-10-16 21:22:07 -0700

    Add solution to LeetCode/search2dMatrix.js

--- LeetCode/search2dMatrix.js
+++ LeetCode/search2dMatrix.js
@@ -30,7 +30,35 @@ Accepted
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+  /* We're going to start at the bottom in terms of the row and init the col to 0. If the target is
+     smaller than the num, we decrement the row. If the target is greater, we increment the col.
+     We do this until we reach the target or go out of bounds.
+  */
 
+  // If either row or column has a length of 0, return false.
+  if (matrix.length === 0 || matrix[0].length === 0) return false;
+
+  // Init the row at the bottom and col at 0.
+  let row = matrix.length - 1;
+  let col = 0;
+
+  let num = matrix[row][col];
+  while(num) {
+    // If our num equals the target, we are done.
+    if (num === target) return true;
+
+    if (num > target) {
+      row--;
+    }
+    else {
+      col++;
+    }
+
+    if (row < 0) return false;
+    num = matrix[row][col];
+  }
+
+  return false;
 };
 
 const matrix = [
@@ -41,6 +69,8 @@ const matrix = [
   [18, 21, 23, 26, 30]
 ];
 
-console.log(searchMatrix(matrix, 5));
+// console.log(searchMatrix(matrix, 5));
 
-console.log(searchMatrix(matrix, 20));
+// console.log(searchMatrix(matrix, 20));
+
+console.log(searchMatrix([[-5]], -10))
