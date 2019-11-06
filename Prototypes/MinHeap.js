8fc5c757042e28f7bcd266a7c70b0cca68f273cc
bwuphan <bwuphan@gmail.com>
2019-11-05 22:10:13 -0800

    Add insert method to Prototypes/MinHeap.js

--- Prototypes/MinHeap.js
+++ Prototypes/MinHeap.js
@@ -1,11 +1,44 @@
-var MinHeap = function(capacity) {
-  this._heap = [];
-  this._capacity = capacity;
+class MinHeap {
+  constructor() {
+    this._heap = [];
+  }
+
+  insert(val) {
+    /*
+      Push the new val to the heap.
+      Set current idx to the last pushed index and get the parent index.
+      If there is no parent, we know we're in the right place and we can return.
+      Otherwise keep swapping until we find the right spot. The inserted val has to be greater
+      than its parent.
+     */
+    this._heap.push(val);
+
+    let thisIdx = this._heap.length - 1;
+    let parentIdx = this.getParentIdx(thisIdx);
+
+    if (parentIdx === null) return val;
+
+    while (parentIdx !== null && this._heap[thisIdx] < this._heap[parentIdx]) {
+      this.swap(parentIdx, thisIdx);
+      thisIdx = parentIdx;
+      parentIdx = this.getParentIdx(thisIdx);
+    }
+  }
+
+  swap(idx1, idx2) {
+    const temp = this._heap[idx1];
+    this._heap[idx1] = this._heap[idx2];
+    this._heap[idx2] = temp;
+  }
+
+  getParentIdx(idx) {
+    const pIdx = Math.floor((idx - 1) / 2);
+    return pIdx < 0 ? null : pIdx;
+  }
 }
 
-MinHeap.prototype.insert = function(val) {
-  this._heap.push(val);
-
-
-}
-
+var minHeap = new MinHeap();
+minHeap.insert(1);
+minHeap.insert(2);
+minHeap.insert(0);
+console.log(minHeap);
