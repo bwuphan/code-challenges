f90d648f5395e9d411e805e0331768d010c0183c
bwuphan <bwuphan@gmail.com>
2019-11-07 20:37:19 -0800

    Add solution for Prototypes/kthLargestElementInAnArray.js

--- Prototypes/kthLargestElementInAnArray.js
+++ Prototypes/kthLargestElementInAnArray.js
@@ -17,6 +17,109 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ array's length.
 */
 
+class MaxHeap {
+  constructor() {
+    this._heap = [];
+  }
+
+  insert(val) {
+    /*
+      Push the new val to the heap.
+      Set current idx to the last pushed index and get the parent index.
+      If there is no parent, we know we're in the right place and we can return.
+      Otherwise keep swapping until we find the right spot. The inserted val has to be greater
+      than its parent.
+     */
+    this._heap.push(val);
+
+    let thisIdx = this._heap.length - 1;
+    let parentIdx = this.getParentIdx(thisIdx);
+
+    if (parentIdx === null) return val;
+
+    while (parentIdx !== null && this._heap[thisIdx] > this._heap[parentIdx]) {
+      this.swap(parentIdx, thisIdx);
+      thisIdx = parentIdx;
+      parentIdx = this.getParentIdx(thisIdx);
+    }
+  }
+
+  swap(idx1, idx2) {
+    const temp = this._heap[idx1];
+    this._heap[idx1] = this._heap[idx2];
+    this._heap[idx2] = temp;
+  }
+
+  getParentIdx(idx) {
+    const pIdx = Math.floor((idx - 1) / 2);
+    return pIdx < 0 ? null : pIdx;
+  }
+
+  peek() {
+    const topOfHeap = this._heap.length ? this._heap[0] : null;
+    console.log('Top:', topOfHeap);
+    return topOfHeap;
+  }
+
+  getLeftChildIdx(idx) {
+    const lIdx = Math.floor((2 * idx) + 1);
+    return lIdx < this._heap.length ? lIdx : null;
+  }
+
+  getRightChildIdx(idx) {
+    const rIdx = Math.floor((2 * idx) + 2);
+    return rIdx < this._heap.length ? rIdx : null;
+  }
+
+  remove() {
+    if (this._heap.length === 0)
+      return null;
+
+    const lastIdx = this._heap.length - 1;
+    this.swap(0, lastIdx);
+
+    const returnVal = this._heap.pop();
+
+    let curIdx, leftChildIdx, rightChildIdx, largerIdx = null;
+
+    curIdx = 0;
+    do {
+
+      let leftChildIdx = this.getLeftChildIdx(curIdx);
+
+      let rightChildIdx = this.getRightChildIdx(curIdx);
+
+      let largerIdx = null;
+
+      // console.log('larger', this._heap, this._heap[curIdx], curIdx)
+      if (rightChildIdx === null || this._heap[leftChildIdx] > this._heap[rightChildIdx]) {
+        // console.log('left')
+        largerIdx = leftChildIdx;
+      }
+      else {
+        // console.log('right');
+        largerIdx = rightChildIdx;
+      }
+      if (this._heap[curIdx] < this._heap[largerIdx]) {
+        this.swap(curIdx, largerIdx);
+        curIdx = largerIdx;
+        continue;
+      }
+      else {
+        break;
+      }
+
+      // console.log('CUR IDX', this._heap[curIdx], curIdx)
+      // console.log('larger IDX', this._heap[largerIdx], largerIdx)
+    }
+    while (true);
+    return returnVal;
+  }
+
+  log() {
+    console.log(this._heap);
+  }
+}
 
 /**
  * @param {number[]} nums
@@ -24,5 +127,22 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
+  let maxHeap = new MaxHeap();
 
+  for (let i = 0; i < nums.length; ++i) {
+    maxHeap.insert(nums[i]);
+  }
+  maxHeap.log();
+  let largest = null;
+  for (let i = 0; i < k; ++i) {
+    // maxHeap.log();
+    largest = maxHeap.remove();
+    // console.log('remove', largest)
+    // maxHeap.log();
+  }
+
+  return largest;
 };
+
+console.log(findKthLargest([3,2,1,5,6,4], 2))
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4))
