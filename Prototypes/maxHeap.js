9caf205246474d543fd85626506771c6c3cb8ebd
bwuphan <bwuphan@gmail.com>
2019-11-08 18:14:26 -0800

    Add Prototypes/maxHeap.js

---
+++ Prototypes/maxHeap.js
@@ -0,0 +1,104 @@
+class MaxHeap {
+  constructor() {
+    this._heap = [];
+  }
+
+  insert(val) {
+    /*
+      Push the new val to the heap.
+      Set current idx to the last pushed index and get the parent index.
+      If there is no parent, we know we're in the right place and we can return.
+      Otherwise keep swapping until we find the right spot. The inserted val has to be greater
+      than its parent.
+     */
+    this._heap.push(val);
+
+    let thisIdx = this._heap.length - 1;
+    let parentIdx = this.getParentIdx(thisIdx);
+
+    if (parentIdx === null) return val;
+
+    while (parentIdx !== null && this._heap[thisIdx] > this._heap[parentIdx]) {
+      this.swap(parentIdx, thisIdx);
+      thisIdx = parentIdx;
+      parentIdx = this.getParentIdx(thisIdx);
+    }
+  }
+
+  swap(idx1, idx2) {
+    const temp = this._heap[idx1];
+    this._heap[idx1] = this._heap[idx2];
+    this._heap[idx2] = temp;
+  }
+
+  getParentIdx(idx) {
+    const pIdx = Math.floor((idx - 1) / 2);
+    return pIdx < 0 ? null : pIdx;
+  }
+
+  peek() {
+    const topOfHeap = this._heap.length ? this._heap[0] : null;
+    console.log('Top:', topOfHeap);
+    return topOfHeap;
+  }
+
+  getLeftChildIdx(idx) {
+    const lIdx = Math.floor((2 * idx) + 1);
+    return lIdx < this._heap.length ? lIdx : null;
+  }
+
+  getRightChildIdx(idx) {
+    const rIdx = Math.floor((2 * idx) + 2);
+    return rIdx < this._heap.length ? rIdx : null;
+  }
+
+  remove() {
+    /*
+      Swap the first element with the last and pop off the new last element.
+      Start at the first element and compare with the higher of its two children.
+      If the larger of the children is larger than the current element, swap the two positions.
+      Keep doing this until you find the right location.
+    */
+
+    if (this._heap.length === 0)
+      return null;
+
+    const lastIdx = this._heap.length - 1;
+    this.swap(0, lastIdx);
+
+    const returnVal = this._heap.pop();
+
+    let curIdx, leftChildIdx, rightChildIdx, largerIdx = null;
+
+    curIdx = 0;
+
+    while (true) {
+      leftChildIdx = this.getLeftChildIdx(curIdx);
+
+      rightChildIdx = this.getRightChildIdx(curIdx);
+
+      largerIdx = null;
+
+      if (rightChildIdx === null || this._heap[leftChildIdx] > this._heap[rightChildIdx]) {
+        largerIdx = leftChildIdx;
+      }
+      else {
+        largerIdx = rightChildIdx;
+      }
+
+      if (this._heap[curIdx] < this._heap[largerIdx]) {
+        this.swap(curIdx, largerIdx);
+        curIdx = largerIdx;
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+    return returnVal;
+  }
+
+  log() {
+    console.log(this._heap);
+  }
+}
