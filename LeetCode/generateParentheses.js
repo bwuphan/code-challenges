0743a66314ca52663f8f774b21a7b5dff712fdac
bwuphan <bwuphan@gmail.com>
2019-11-16 13:19:00 -0800

    Add LeetCode/generateParentheses.js

---
+++ LeetCode/generateParentheses.js
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/generate-parentheses/
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed
+parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+
+};
