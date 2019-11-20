c65be723fd0aca31deccd7dcf07811f873cb4acd
bwuphan <bwuphan@gmail.com>
2019-11-19 20:21:23 -0800

    Add solution for LeetCode/timeBasedKeyValueStore.js

--- LeetCode/timeBasedKeyValueStore.js
+++ LeetCode/timeBasedKeyValueStore.js
@@ -61,6 +61,35 @@ var TimeMap = function() {
  * @return {void}
  */
 TimeMap.prototype.set = function(key, value, timestamp) {
+  /*
+    Each value of the storage map will be a "bucket."
+    Each bucket will have an array where all the values w/ timestamps are stored.
+      { value, timestamp }
+
+    Each bucket will have a map where the key is a timestamp and the value is the index in which
+      this particular timestamp exists in the bucket array.
+  */
+
+  // If the storage doesn't already contain the key, we need to create new bucket.
+  if (!this._storage.has(key)) {
+    const map = new Map();
+    map.set(timestamp, 0);
+    const newBucket = {
+      map,
+      arr: [{ value, timestamp }],
+    }
+    this._storage.set(key, newBucket);
+  }
+  // Else, add to the bucket.
+  else {
+    const bucket = this._storage.get(key);
+
+    // Set the map value to the new index and push to the array.
+    bucket.map.set(timestamp, bucket.arr.length);
+    bucket.arr.push({ value, timestamp });
+
+    this._storage.set(key, bucket);
+  }
 
 };
 
@@ -70,8 +99,55 @@ TimeMap.prototype.set = function(key, value, timestamp) {
  * @return {string}
  */
 TimeMap.prototype.get = function(key, timestamp) {
+  /*
+    Basic strategy is if the key exists and we do not have an exact timestamp match, we will find
+    the value we need by using a binary search. From that point, if the timestamp is greater than
+    that point, we can return that point. In the case that the timestamp is less than the found
+    point, we can return the next lowest (to the left of the array).
+  */
 
+  // if the storage doesn't contain the key, return null.
+  if (!this._storage.has(key)) return null;
+
+  // Get the bucket.
+  const bucket = this._storage.get(key);
+
+  // If the bucket array length is only 1, we don't need to search.
+  if (bucket.arr.length === 1)
+    return (bucket.arr[0].timestamp === timestamp || bucket.arr[0].timestamp < timestamp) ? bucket.arr[0].value : null;
+
+  // If the timestamp exists exactly in the bucket, we can just return this value.
+  if (bucket.map.has(timestamp))
+    return bucket.arr[bucket.map.get(timestamp)].value;
+
+
+  // Binary Search
+  let start = 0;
+  let end = bucket.arr.length - 1;
+
+  // Go until start and end are the same value.
+  while (start < end) {
+    const midpointIdx = Math.floor((start + end) / 2);
+    const midpointVal = bucket.arr[midpointIdx];
+
+    // If the timestamp is less than the midpoint, search to the left.
+    if (timestamp < midpointVal.timestamp) {
+      end = midpointIdx - 1;
+    }
+    // Else, search to the right.
+    else if (timestamp > midpointVal.timestamp) {
+      start = midpointIdx + 1;
+    }
+  }
+
+  // If the start is 0, we were not able to find the value.
+  if (start === 0) return '';
+  // If the timestamp greater than the found idx, return the found idx.
+  if (timestamp > bucket.arr[start].timestamp) return bucket.arr[start].value;
+  // Else, timestamp must be less than the found idx in which case, return one to the left.
+  return bucket.arr[start - 1].value;
 };
+
 
 /**
  * Your TimeMap object will be instantiated and called as such:
@@ -79,3 +155,32 @@ TimeMap.prototype.get = function(key, timestamp) {
  * obj.set(key,value,timestamp)
  * var param_2 = obj.get(key,timestamp)
  */
+
+let kv = new TimeMap();
+// kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1
+// console.log(kv.get("foo", 1));  // output "bar"
+// console.log(kv.get("foo", 3)); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"
+// kv.set("foo", "bar2", 4);
+// kv.set("foo", "bar3", 5); // store the key "foo" and value "bar" along with timestamp = 1
+// kv.set("foo", "bar4", 7); // store the key "foo" and value "bar" along with timestamp = 1
+// console.log(kv.get("foo", 4)); // output "bar2"
+// console.log(kv.get("foo", 5)); // output "bar2"
+// console.log(kv.get("foo", 6)); //output "bar2"
+// console.log(kv.get("foo", 10)); //output "bar2"
+
+// kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1
+// console.log(kv.get("foo", 1));  // output "bar"
+// console.log(kv.get("foo", 3)); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"
+// kv.set("foo", "bar2", 4);
+// console.log(kv.get("foo", 4)); // output "bar2"
+// console.log(kv.get("foo", 5)); //output "bar2"
+
+
+kv.set("love", "high", 10);
+kv.set("love", "low", 20);
+console.log(kv.get("love", 5));
+console.log(kv.get("love", 10))
+console.log(kv.get("love", 15))
+console.log(kv.get("love", 20))
+console.log(kv.get("love", 25))
+console.log(kv.get())
