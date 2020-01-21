c426868297b09827091428344c740101c8419eb6
bwuphan <bwuphan@gmail.com>
2020-01-20 17:51:37 -0800

    Fully implement LeetCode/implementTrie.js

--- LeetCode/implementTrie.js
+++ LeetCode/implementTrie.js
@@ -19,41 +19,117 @@ You may assume that all inputs are consist of lowercase letters a-z.
 All inputs are guaranteed to be non-empty strings.
 */
 
+class Node {
+  constructor(letter, endOfWord) {
+    this.letter = letter;
+    this.children = new Map();
+    this.endOfWord = endOfWord;
+  }
 
-/**
- * Initialize your data structure here.
- */
-var Trie = function() {
+  getLetter() {
+    return this.letter;
+  }
 
-};
 
-/**
- * Inserts a word into the trie.
- * @param {string} word
- * @return {void}
- */
-Trie.prototype.insert = function(word) {
+  addChild(node) {
+    this.children.set(node.getLetter(), node);
+  }
 
-};
+  getChild(letter) {
+    return this.children.get(letter);
+  }
+}
 
-/**
- * Returns if the word is in the trie.
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function(word) {
 
-};
+class Trie {
+  constructor() {
+    // Init root to an empty node.
+    this.root = new Node(null, false);
+  }
 
-/**
- * Returns if there is any word in the trie that starts with the given prefix.
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
+  /**
+   * Inserts a word into the trie.
+   * @param {string} word
+   * @return {void}
+   */
+  insert(word) {
+    let curNode = this.root;
 
-};
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
 
+      const curNodeChild = curNode.getChild(letter);
+
+      // If there is a curNodeChild, set the curNode to that child.
+      if (curNodeChild) {
+        curNode = curNodeChild;
+
+        // If we are at the end of the inserted word, then we know that this new curNode is also
+        // and endOfWord.
+        if (i === word.length - 1) curNode.endOfWord = true;
+      }
+      // Else, we are going to make a new child node and set curNode to this new child node.
+      else {
+        const newChildNode = new Node(letter, i === (word.length - 1));
+        curNode.addChild(newChildNode);
+        curNode = newChildNode;
+      }
+    }
+  }
+  /**
+   * Returns if the word is in the trie.
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    let curNode = this.root;
+
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
+
+      const curNodeChild = curNode.getChild(letter);
+
+      if (curNodeChild) {
+        if (i === (word.length - 1) && curNodeChild.endOfWord) return true;
+        else curNode = curNodeChild;
+      }
+      else return false;
+    }
+
+    return false;
+  }
+
+  /**
+   * Returns if there is any word in the trie that starts with the given prefix.
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    // Find node for last letter of prefix.
+    let curNode = this.root;
+    for (let i = 0; i < prefix.length; ++i) {
+      const letter = prefix[i];
+
+      const curNodeChild = curNode.getChild(letter);
+      if (curNodeChild) curNode = curNodeChild;
+      else return false;
+    }
+
+    // DFS through until we find a node that is an endOfWord.
+    let stack = [curNode];
+    curNode = stack.pop();
+
+    while (curNode) {
+      if (curNode.endOfWord) return true;
+
+      curNode.children.forEach(n => stack.push(n));
+
+      curNode = stack.pop();
+    }
+
+    return false;
+  }
+}
 /**
  * Your Trie object will be instantiated and called as such:
  * var obj = new Trie()
@@ -61,3 +137,18 @@ Trie.prototype.startsWith = function(prefix) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+var trie = new Trie();
+
+trie.insert("apple");
+console.log(trie.search("apple"));   // returns true
+console.log(trie.search("app"));     // returns false
+console.log(trie.startsWith("apples")); // returns true
+trie.insert("app");
+console.log(trie.search("app"));     // returns true
+
+// console.log('FINAl', trie.root.children);
+
+var node = new Node('e', true);
+
+// console.log(node);
