7bca6e3a3ecfc5639a2ca81793a53db500500c76
bwuphan <bwuphan@gmail.com>
2020-01-23 18:30:32 -0800

    Add LeetCode/searchSuggestionsSystem.js

---
+++ LeetCode/searchSuggestionsSystem.js
@@ -0,0 +1,168 @@
+/*
+https://leetcode.com/problems/search-suggestions-system/
+
+Given an array of strings products and a string searchWord. We want to design a system that suggests
+at most three product names from products after each character of searchWord is typed. Suggested
+products should have common prefix with the searchWord. If there are more than three products with
+a common prefix return the three lexicographically minimums products.
+
+Return list of lists of the suggested products after each character of searchWord is typed.
+
+
+
+Example 1:
+
+Input: products = ["mobile","mouse","moneypot","monitor","mousepad"], searchWord = "mouse"
+Output: [
+["mobile","moneypot","monitor"],
+["mobile","moneypot","monitor"],
+["mouse","mousepad"],
+["mouse","mousepad"],
+["mouse","mousepad"]
+]
+Explanation: products sorted lexicographically = ["mobile","moneypot","monitor","mouse","mousepad"]
+After typing m and mo all products match and we show user ["mobile","moneypot","monitor"]
+After typing mou, mous and mouse the system suggests ["mouse","mousepad"]
+Example 2:
+
+Input: products = ["havana"], searchWord = "havana"
+Output: [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+Example 3:
+
+Input: products = ["bags","baggage","banner","box","cloths"], searchWord = "bags"
+Output: [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
+Example 4:
+
+Input: products = ["havana"], searchWord = "tatiana"
+Output: [[],[],[],[],[],[],[]]
+
+
+Constraints:
+
+1 <= products.length <= 1000
+There are no repeated elements in products.
+1 <= Σ products[i].length <= 2 * 10^4
+All characters of products[i] are lower-case English letters.
+1 <= searchWord.length <= 1000
+All characters of searchWord are lower-case English letters.
+*/
+class Node {
+  constructor(letter, endOfWord) {
+    this.letter = letter;
+    this.children = new Map();
+    this.endOfWord = endOfWord;
+  }
+
+  getLetter() {
+    return this.letter;
+  }
+
+
+  addChild(node) {
+    this.children.set(node.getLetter(), node);
+  }
+
+  getChild(letter) {
+    return this.children.get(letter);
+  }
+}
+
+
+class Trie {
+  constructor() {
+    // Init root to an empty node.
+    this.root = new Node(null, false);
+  }
+
+  /**
+   * Inserts a word into the trie.
+   * @param {string} word
+   * @return {void}
+   */
+  insert(word) {
+    let curNode = this.root;
+
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
+
+      const curNodeChild = curNode.getChild(letter);
+
+      // If there is a curNodeChild, set the curNode to that child.
+      if (curNodeChild) {
+        curNode = curNodeChild;
+
+        // If we are at the end of the inserted word, then we know that this new curNode is also
+        // and endOfWord.
+        if (i === word.length - 1) curNode.endOfWord = true;
+      }
+      // Else, we are going to make a new child node and set curNode to this new child node.
+      else {
+        const newChildNode = new Node(letter, i === (word.length - 1));
+        curNode.addChild(newChildNode);
+        curNode = newChildNode;
+      }
+    }
+  }
+  /**
+   * Returns if the word is in the trie.
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    let curNode = this.root;
+
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
+
+      const curNodeChild = curNode.getChild(letter);
+
+      if (curNodeChild) {
+        if (i === (word.length - 1) && curNodeChild.endOfWord) return true;
+        else curNode = curNodeChild;
+      }
+      else return false;
+    }
+
+    return false;
+  }
+
+  /**
+   * Returns if there is any word in the trie that starts with the given prefix.
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    // Find node for last letter of prefix.
+    let curNode = this.root;
+    for (let i = 0; i < prefix.length; ++i) {
+      const letter = prefix[i];
+
+      const curNodeChild = curNode.getChild(letter);
+      if (curNodeChild) curNode = curNodeChild;
+      else return false;
+    }
+
+    // DFS through until we find a node that is an endOfWord.
+    let stack = [curNode];
+    curNode = stack.pop();
+
+    while (curNode) {
+      if (curNode.endOfWord) return true;
+
+      curNode.children.forEach(n => stack.push(n));
+
+      curNode = stack.pop();
+    }
+
+    return false;
+  }
+}
+
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+var suggestedProducts = function(products, searchWord) {
+
+};
