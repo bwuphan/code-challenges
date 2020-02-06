b447b6107cfd0bff5d6881295b83b0dd5cc3fa58
bwuphan <bwuphan@gmail.com>
2020-02-05 19:19:01 -0800

    Start work on wordSearch2 but with a trie

--- LeetCode/wordSearch2.js
+++ LeetCode/wordSearch2.js
@@ -1,7 +1,10 @@
 /*
+https://leetcode.com/problems/word-search-ii/
+
 Given a 2D board and a list of words from the dictionary, find all words in the board.
 
-Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells
+are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
 
 
 
@@ -25,76 +28,135 @@ All inputs are consist of lowercase letters a-z.
 The values of words are distinct.
 */
 
+class Node {
+  constructor(letter, endOfWord) {
+    this.letter = letter;
+    this.children = new Map();
+    this.endOfWord = endOfWord;
+    this.wordOccurences = {};
+  }
+
+  getLetter() {
+    return this.letter;
+  }
+
+
+  addChild(node) {
+    this.children.set(node.getLetter(), node);
+  }
+
+  getChild(letter) {
+    return this.children.get(letter);
+  }
+}
+
+
+class Trie {
+  constructor() {
+    // Init root to an empty node.
+    this.root = new Node(null, false);
+  }
+
+  /**
+   * Inserts a word into the trie.
+   * @param {string} word
+   * @return {void}
+   */
+  insert(word) {
+    let curNode = this.root;
+
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
+
+      const curNodeChild = curNode.getChild(letter);
+
+      // If there is a curNodeChild, set the curNode to that child.
+      if (curNodeChild) {
+        curNode = curNodeChild;
+
+        if (word in curNode.wordOccurences)
+          curNode.wordOccurences[word]++;
+        else
+          curNode.wordOccurences[word] = 1;
+
+        // If we are at the end of the inserted word, then we know that this new curNode is also
+        // and endOfWord.
+        if (i === word.length - 1) curNode.endOfWord = true;
+      }
+      // Else, we are going to make a new child node and set curNode to this new child node.
+      else {
+        const newChildNode = new Node(letter, i === (word.length - 1));
+        newChildNode.wordOccurences[word] = 1;
+        curNode.addChild(newChildNode);
+        curNode = newChildNode;
+      }
+    }
+  }
+  /**
+   * Returns if the word is in the trie.
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    let curNode = this.root;
+
+    for (let i = 0; i < word.length; ++i) {
+      const letter = word[i];
+
+      const curNodeChild = curNode.getChild(letter);
+
+      if (curNodeChild) {
+        if (i === (word.length - 1) && curNodeChild.endOfWord) return true;
+        else curNode = curNodeChild;
+      }
+      else return false;
+    }
+
+    return false;
+  }
+
+  /**
+   * Returns if there is any word in the trie that starts with the given prefix.
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    // Find node for last letter of prefix.
+    let curNode = this.root;
+    for (let i = 0; i < prefix.length; ++i) {
+      const letter = prefix[i];
+
+      const curNodeChild = curNode.getChild(letter);
+      if (curNodeChild) curNode = curNodeChild;
+      else return false;
+    }
+
+    // DFS through until we find a node that is an endOfWord.
+    let stack = [curNode];
+    curNode = stack.pop();
+
+    while (curNode) {
+      if (curNode.endOfWord) return true;
+
+      curNode.children.forEach(n => stack.push(n));
+
+      curNode = stack.pop();
+    }
+
+    return false;
+  }
+}
+
+
 /**
  * @param {character[][]} board
  * @param {string[]} words
  * @return {string[]}
  */
 var findWords = function(board, words) {
-  let results = [];
+  const trie = new Trie();
+  words.forEach(word => trie.insert(word));
 
-  const traverse = function(word, idx, x, y, board) {
-    // If it's out of bounds, return.
-    if (y >= board.length || y < 0) {
-      return;
-    }
-    if (x >= board[0].length || x < 0) {
-      return;
-    }
-
-    // If the current node is null, return (This means that it has been visited).
-    if (board[y][x] === null) {
-      return;
-    }
-
-    // If there is a match between the current letter and the letter at the current node.
-    if (board[y][x] === word[idx]) {
-      // Make a copy of the board.
-      copiedBoard = copy2dArray(board);
-
-      // Mark the node as visited.
-      copiedBoard[y][x] = null;
-
-      idx++;
-
-      // If the index is now greated or equal to the length of the word, we are done.
-      if (idx >= word.length) {
-        results.push(word);
-        removeFromWords(word, words);
-      }
-
-      traverse(word, idx, x + 1, y, copiedBoard);
-      traverse(word, idx, x - 1, y, copiedBoard);
-      traverse(word, idx, x, y + 1, copiedBoard);
-      traverse(word, idx, x, y - 1, copiedBoard);
-    }
-  }
-
-  for (let i = 0; i < board.length; ++i) {
-    for (let j = 0; j < board[0].length; ++j) {
-      words.forEach(word => {
-        if (board[i][j] === word[0]) {
-          traverse(word, 0, j, i, board);
-        }
-      });
-    }
-  }
-  return results;
-};
-
-function copy2dArray(arr) {
-  return arr.map(arr => arr.slice());
-}
-
-function removeFromWords(word, words) {
-  let idx = null;
-  for (let i = 0; i < words.length; ++i) {
-    if (word === words[i]) {
-      idx = i;
-      break;
-    }
-  }
-  words.splice(idx, 1);
 }
 
 const board = [
@@ -102,20 +164,6 @@ const board = [
   ['e','t','a','e'],
   ['i','h','k','r'],
   ['i','f','l','v']
-]
-const words = ["oath","pea","eat","rain"]
+];
 
-// console.log(findWords(board, words));
-
-const board2 = [
-  ["a","b","c"],
-  ["a","e","d"],
-  ["a","f","g"]
-]
-const words2 = ["abcdefg","gfedcbaaa","eaabcdgfa","befa","dgc","ade"]
-
-console.log(findWords(board2, words2))
-
-board3 = copy2dArray(board2);
-board3[0][0] = null;
-console.log(board2, board3);
+console.log(findWords(board, ["oath","pea","eat","rain"]))
