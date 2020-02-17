b4103486363f6824f5afb1b95604358e9e1de5de
bwuphan <bwuphan@gmail.com>
2020-02-17 14:58:33 -0800

    Add test case 1 for LeetCode/lfuCache/lfuCacheTest.js

--- LeetCode/lfuCache/lfuCacheTest.js
+++ LeetCode/lfuCache/lfuCacheTest.js
@@ -1,17 +1,17 @@
 describe('lfuCache', () => {
-  const LfuCache = require('./lfuCache.js').LfuCache;
+  const LFUCache = require('./lfuCache.js').LFUCache;
 
   it('passes test case 1.', () => {
-    const cache = new LfuCache(2);
+    const cache = new LFUCache(2);
     cache.put(1, 1);
     cache.put(2, 2);
-    cache.get(1);       // returns 1
+    expect(cache.get(1)).toBe(1);       // returns 1
     cache.put(3, 3);    // evicts key 2
-    cache.get(2);       // returns -1 (not found)
-    cache.get(3);       // returns 3.
+    expect(cache.get(2)).toBe(2);       // returns -1 (not found)
+    expect(cache.get(3)).toBe(3);       // returns 3.
     cache.put(4, 4);    // evicts key 1.
-    cache.get(1);       // returns -1 (not found)
-    cache.get(3);       // returns 3
-    cache.get(4);       // returns 4
+    expect(cache.get(1)).toBe(1);       // returns -1 (not found)
+    expect(cache.get(3)).toBe(3);       // returns 3
+    expect(cache.get(4)).toBe(4);       // returns 4
   })
 });
