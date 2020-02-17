115d163b34cdebf3aea4d549611b23deee506054
bwuphan <bwuphan@gmail.com>
2020-02-17 14:59:51 -0800

    Add LeetCode/constructBinaryTreeFromInorderPreorderTraversal/constructBinaryTreeFromInorderPreorderTraversal.js

---
+++ LeetCode/constructBinaryTreeFromInorderPreorderTraversal/constructBinaryTreeFromInorderPreorderTraversal.js
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+
+};
