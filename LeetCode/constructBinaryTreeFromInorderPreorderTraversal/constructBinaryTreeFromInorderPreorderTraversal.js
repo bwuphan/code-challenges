75c8a1c7b5a8dcea008fc3691f4d1008a6162aab
bwuphan <bwuphan@gmail.com>
2020-02-17 18:30:05 -0800

    Add solution for LeetCode/constructBinaryTreeFromInorderPreorderTraversal/constructBinaryTreeFromInorderPreorderTraversal.js

--- LeetCode/constructBinaryTreeFromInorderPreorderTraversal/constructBinaryTreeFromInorderPreorderTraversal.js
+++ LeetCode/constructBinaryTreeFromInorderPreorderTraversal/constructBinaryTreeFromInorderPreorderTraversal.js
@@ -32,5 +32,37 @@ Return the following binary tree:
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
+  // Create an inorder map to keep track of indices for fast access.
+  const inorderMap = {};
+  inorder.forEach((val, i) => inorderMap[val] = i);
 
+  // Init the preorderIdx to the first element. Every time we create a new node, we increment this.
+  let preorderIdx = 0;
+
+  const dfs = (preLeftIdx, preRightIdx) => {
+    // If the left idx is greater than the right idx, we have gone too far and need to return null.
+    if (preLeftIdx > preRightIdx)
+      return null;
+
+    // Get the value of the new node we are going to create by taking the val at the preorderIdx.
+    // After we take this value, we increment preorderIdx for the next node we are creating.
+    const curVal = preorder[preorderIdx];
+    preorderIdx++;
+
+    // Create the new node.
+    const node = new TreeNode(curVal);
+
+    // Get the index of the curVal in the inorder array. We use this to split the inorder array
+    // into the left and right subtrees for this node.
+    const inorderIdx = inorderMap[curVal];
+
+    // We have to go left first since we are going preorder. Left happens first.
+    node.left = dfs(preLeftIdx, inorderIdx -1);
+
+    node.right = dfs(inorderIdx + 1, preRightIdx);
+
+    return node;
+  }
+
+  return dfs(0, preorder.length - 1);
 };
