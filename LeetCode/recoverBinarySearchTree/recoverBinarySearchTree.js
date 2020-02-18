1dd454406c465132eb04db03f59aa0762679dbdc
bwuphan <bwuphan@gmail.com>
2020-02-18 00:01:16 -0800

    Add solution for LeetCode/recoverBinarySearchTree/recoverBinarySearchTree.js

--- LeetCode/recoverBinarySearchTree/recoverBinarySearchTree.js
+++ LeetCode/recoverBinarySearchTree/recoverBinarySearchTree.js
@@ -45,6 +45,7 @@ A solution using O(n) space is pretty straight forward.
 Could you devise a constant space solution?
 */
 
+var arrayToTree = require('../../Util/arrayToTree.js').arrayToTree;
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -72,14 +73,13 @@ var recoverTree = function(root) {
 
   inorderDfs(root);
 
-  console.log(inorderArr);
   let node1, idxNode1, node2 = null;
 
   let pastVal = inorderArr[0].val;
   for (let i = 0; i < inorderArr.length; ++i) {
     const curNode = inorderArr[i];
 
-    if ((inorderArr[i - 1] && curNode.val < inorderArr[i - 1].val) ||
+    if ((inorderArr[i - 1] && !inorderArr[i + 1] && curNode.val < inorderArr[i - 1].val) ||
         (inorderArr[i + 1] && curNode.val > inorderArr[i + 1].val)) {
       node1 = curNode;
       idxNode1 = i;
@@ -92,9 +92,11 @@ var recoverTree = function(root) {
     const curNode = inorderArr[i];
 
     if (i === idxNode1 + 1) {
-      if ((!inorderArr[i + 1] && curNode.val < node1.val) || (inorderArr[i + 1] && curNode.val < inorderArr[i + 1].val)) {
-        node2 = curNode;
-        break;
+      if ((!inorderArr[i + 1] && curNode.val < node1.val) ||
+          (curNode.val <= node1.val && node1.val <= inorderArr[i + 1].val)) {
+
+          node2 = curNode;
+          break;
       }
     }
     else if (curNode.val < inorderArr[i - 1].val) {
@@ -102,7 +104,6 @@ var recoverTree = function(root) {
       break;
     }
   }
-
   if (node1 && node2) {
     let tempVal = node1.val;
     node1.val = node2.val;
@@ -116,84 +117,31 @@ var recoverTree = function(root) {
      this.left = this.right = null;
  }
 
-// const head = new TreeNode(1);
-// head.left = new TreeNode(3);
-// head.left.right = new TreeNode(2);
+const head = new TreeNode(1);
+head.left = new TreeNode(3);
+head.left.right = new TreeNode(2);
 
-const head = new TreeNode(0);
-head.left = new TreeNode(1);
-
+// const head = arrayToTree([3,1,4,null,null,2]);
 console.log(recoverTree(head));
 
-var Node = function(val) {
-  this.val = val;
+const inorderArr = [];
+const inorderDfs = (node) => {
+  if (!node) {
+    inorderArr.push(null);
+    return;
+  }
 
-  this.next = null;
+  inorderDfs(node.left);
+
+  inorderArr.push(node);
+
+  inorderDfs(node.right);
 }
 
-class Queue {
-  constructor() {
-    this._first = null;
-    this._last = null;
-
-    this._size = 0;
-  }
-
-  enqueue(val) {
-    const node = new Node(val);
-    if (!this._first) {
-      this._first = node;
-      this._last = this._first;
-    }
-    else {
-      this._last.next = node;
-      this._last = node;
-    }
-
-    this._size++;
-  }
-
-  dequeue() {
-    if (!this._first) return null;
-
-    const dequeued = this._first;
-
-    if (this._first === this._last) {
-      this._first = null;
-      this._tail = null;
-    }
-    else {
-      this._first = dequeued.next;
-    }
-
-    this._size--;
-
-    return dequeued.val;
-  }
-
-  peek() {
-    if (!this._first) return null;
-
-    return this._first.val;
-  }
-
-  isEmpty() {
-    return this._first === null;
-  }
-
-  log() {
-    let curNode = this._first;
-
-    let qString = '';
-    while (curNode) {
-      qString += `${curNode.val}, `;
-      curNode = curNode.next;
-    }
-
-    console.log(`Queue: ${qString}`);
-  }
-}
-
-function arrayToTree(array) {
-
-}
+inorderDfs(recoverTree(head));
+inorderArr.forEach((node, i) => {
+  if (node)
+    console.log(i, node.val)
+  else
+    console.log(i, null);
+})
