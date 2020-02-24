6aaea8321db9304adf16917970c3ce2c3a6f696a
bwuphan <bwuphan@gmail.com>
2020-02-23 18:37:36 -0800

    Add verticalOrderTraversalOfBinaryTreeTest

---
+++ LeetCode/verticalOrderTraversalOfBinaryTreeTest/verticalOrderTraversalOfBinaryTreeTest.js
@@ -0,0 +1,9 @@
+describe('verticalOrderTraversalOfBinaryTree', () => {
+  const arrayToTree = require('../../Util/arrayToTree.js').arrayToTree;
+  const verticalTraversal = require('./verticalOrderTraversalOfBinaryTree.js').verticalTraversal;
+
+  it('passes test case for [3,9,20,null,null,15,7].', () => {
+    const tree = arrayToTree([3,9,20,null,null,15,7]);
+    expect(verticalTraversal(tree)).toBe([[9],[3,15],[20],[7]]);
+  });
+});
