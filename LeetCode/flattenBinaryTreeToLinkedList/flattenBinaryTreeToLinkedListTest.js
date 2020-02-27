73b9bb512277375b239e6507c48923f2347c58b5
bwuphan <bwuphan@gmail.com>
2020-02-26 21:34:53 -0800

    Add flattenBinaryTreeToLinkedList/

---
+++ LeetCode/flattenBinaryTreeToLinkedList/flattenBinaryTreeToLinkedListTest.js
@@ -0,0 +1,4 @@
+describe('flattenBinaryTreeToLinkedList', () => {
+  const arrayToTree = require('../../Util/arrayToTree.js').arrayToTree;
+  const flatten = require('./flattenBinaryTreeToLinkedList.js').flatten;
+});
