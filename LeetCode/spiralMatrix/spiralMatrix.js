9ef710d5a8c8a3e66f08b20aec3d7ad2427a1987
bwuphan <bwuphan@gmail.com>
2020-03-04 21:30:38 -0800

    Add LeetCode/spiralMatrix/

---
+++ LeetCode/spiralMatrix/spiralMatrix.js
@@ -0,0 +1,49 @@
+/*
+https://leetcode.com/problems/spiral-matrix/
+
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral
+order.
+
+Example 1:
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+
+};
+
+const input1 = [
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+];
+
+console.log(spiralOrder(input1));
+
+// const input2 = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9,10,11,12]
+// ];
+
+// console.log(spiralOrder(input2));
