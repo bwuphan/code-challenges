a76114f837804aee351674fd1cc6eec6f0522b10
bwuphan <bwuphan@gmail.com>
2020-03-04 22:15:52 -0800

    Work in progress spiralMatrix.js

--- LeetCode/spiralMatrix/spiralMatrix.js
+++ LeetCode/spiralMatrix/spiralMatrix.js
@@ -29,7 +29,32 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
+  let startCol = 0;
+  let endCol = matrix[0].length - 1;
+  let startRow = 0;
+  let endRow = matrix.length - 1;
+  let results = [];
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i < endCol; i++) {
+      results.push(matrix[startRow][i])
+    }
+    for (let i = startRow; i < endRow; i++) {
+      results.push(matrix[i][endRow]);
+    }
+    for (let i = endCol; i >= startCol; i--) {
+      results.push(matrix[endRow][i]);
+    }
+    endCol--;
+    startRow++;
+    endRow--;
+    for (let i = endRow; i >= startRow; i--) {
+      results.push(matrix[i][startCol]);
+    }
+    startCol++;
+    // console.log(results);
+  }
 
+  return results;
 };
 
 const input1 = [
@@ -40,10 +65,10 @@ const input1 = [
 
 console.log(spiralOrder(input1));
 
-// const input2 = [
-//   [1, 2, 3, 4],
-//   [5, 6, 7, 8],
-//   [9,10,11,12]
-// ];
+const input2 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+];
 
-// console.log(spiralOrder(input2));
+console.log(spiralOrder(input2));
