b8fe2cda82bed4c2fdebe2172e9fdc480462ceff
bwuphan <bwuphan@gmail.com>
2020-03-15 14:49:51 -0700

    Add reorderLinkedListTest.js

---
+++ LeetCode/reorderLinkedList/reorderLinkedListTest.js
@@ -0,0 +1,10 @@
+describe('reorderLinkedList', () => {
+  const reorderList = require('./reorderLinkedList.js').reorderList;
+  const linkedListToArray = require('../../Util/linkedListToArray.js').linkedListToArray;
+  const arrayToLinkedList = require('../../Util/arrayToLinkedList.js').arrayToLinkedList;
+
+
+  it('passes test case 1', () => {
+    const linkedList = arrayToLinkedList([1,2,3,4]);
+  });
+});
