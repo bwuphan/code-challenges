ddaa8e5ecc549be9b587fd8759791c96d0d4686a
bwuphan <bwuphan@gmail.com>
2020-03-22 14:08:56 -0700

    Add addTwoNumbers2Test.js

---
+++ LeetCode/addTwoNumbers2/addTwoNumbers2Test.js
@@ -0,0 +1,12 @@
+describe('addTwoNumbers', () => {
+  const addTwoNumbers = require('./addTwoNumbers2.js').addTwoNumbers;
+  console.log(addTwoNumbers);
+  const arrayToLinkedList = require('../../Util/arrayToLinkedList.js').arrayToLinkedList;
+  const linkedListToArray = require('../../Util/linkedListToArray.js').linkedListToArray;
+
+  it('passes test case 1.', () => {
+    const one = arrayToLinkedList([7,2,4,3]);
+    const two = arrayToLinkedList([5,6,4]);
+    expect(linkedListToArray(addTwoNumbers(one, two))).toEqual([7,8,0,7]);
+  });
+});
