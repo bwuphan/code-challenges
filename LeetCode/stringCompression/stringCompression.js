53b1d9d61bf9b0fd9dd6e9df29270180cb9b56b3
bwuphan <bwuphan@gmail.com>
2020-03-23 23:52:10 -0700

    Add solution for stringCompression.js

--- LeetCode/stringCompression/stringCompression.js
+++ LeetCode/stringCompression/stringCompression.js
@@ -19,5 +19,31 @@ Could you solve it using only O(1) extra space?
  * @return {number}
  */
 var compress = function(chars) {
-
+  let lastChar = null;
+  for (let i = 0; i < chars.length; ++i) {
+    const curChar = chars[i];
+    if (lastChar !== curChar) {
+      let numLetters = 1;
+      for (let j = i + 1; j < chars.length; ++j) {
+        const innerCurChar = chars[j];
+        if (innerCurChar === curChar)
+          numLetters++;
+        else
+          break;
+      }
+      if (numLetters > 1) {
+        const numLettersArr = `${numLetters}`.split('');
+        // Use apply for cases like '12' since it has to be set as '1', '2'.
+        chars.splice.apply(chars, [i + 1, numLetters - 1].concat(numLettersArr));
+        i++;
+      }
+    }
+    lastChar = curChar;
+  }
+  return chars.length;
 };
+/*
+Solution:
+Have outer loop and inner loop that starts the search for the number of same characters when the
+character changes.
+*/
