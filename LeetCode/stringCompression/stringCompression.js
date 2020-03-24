945689bb0d73fec11f197505e3d9ba0d0f40590d
bwuphan <bwuphan@gmail.com>
2020-03-23 22:53:27 -0700

    Add stringCompression

---
+++ LeetCode/stringCompression/stringCompression.js
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/string-compression/
+
+Given an array of characters, compress it in-place.
+
+The length after compression must always be smaller than or equal to the original array.
+
+Every element of the array should be a character (not int) of length 1.
+
+After you are done modifying the input array in-place, return the new length of the array.
+
+
+Follow up:
+Could you solve it using only O(1) extra space?
+*/
+
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+
+};
