ed02cbef0d312663016d2e3524d3f44b5d357988
bwuphan <bwuphan@gmail.com>
2020-03-29 11:04:01 -0700

    Add LeetCode/cropWords.js

---
+++ LeetCode/cropWords.js
@@ -0,0 +1,12 @@
+/*
+Microsoft Online Assessment
+
+https://leetcode.com/discuss/interview-question/507367/
+*/
+
+const cropWords = (message, K) => {
+
+}
+
+
+console.log(cropWords('Codility We test coders') === 'Codility We');
