c962b6612ae1f7a3973bc5d38a9e08293b6dc1fa
bwuphan <bwuphan@gmail.com>
2020-03-31 20:53:26 -0700

    Add largestAlphabetCharacter.js

---
+++ LeetCode/largestAlphabetCharacter.js
@@ -0,0 +1,13 @@
+/*
+Microsoft Online Assessment
+
+https://leetcode.com/discuss/interview-question/548119/
+
+Given a string S, find the largest alphabetic character, whose both uppercase and lowercase appear
+in S. The uppercase character should be returned. For example, for S = "admeDCAB", return "D". If
+there is no such character, return "NO".
+*/
+
+const largestAlphabetCharacter = (S) => {
+
+}
