b6cdd1bc223f16fa2695f57da08f05f9ee47fcd0
bwuphan <bwuphan@gmail.com>
2020-04-01 22:47:21 -0700

    Add largestInteger.js

---
+++ LeetCode/largestInteger.js
@@ -0,0 +1,21 @@
+/*
+Microsoft Online Assessment
+
+https://leetcode.com/discuss/interview-question/406031/
+
+Write a function that, given an array A of N integers, returns the lagest integer K > 0 such that
+both values K and -K exisit in array A. If there is no such integer, the function should return 0.
+
+Example 1:
+
+Input: [3, 2, -2, 5, -3]
+Output: 3
+Example 2:
+
+Input: [1, 2, 3, -4]
+Output: 0
+*/
+
+const largestInteger = (numbers) => {
+
+}
