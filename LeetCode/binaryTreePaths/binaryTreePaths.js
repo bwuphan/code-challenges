2783d7831e333401b597c68a1d934bc32a6f5ef0
bwuphan <bwuphan@gmail.com>
2020-04-16 22:46:30 -0700

    Add binaryTreePaths.js

---
+++ LeetCode/binaryTreePaths/binaryTreePaths.js
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/binary-tree-paths/
+
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+
+};
