cb0d5b500d5ef9c8c7e9a3ed5ddbfc9a59987fb1
bwuphan <bwuphan@gmail.com>
2020-04-19 18:46:53 -0700

    Add maximalSquare.js

---
+++ LeetCode/maximalSquare.js
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/maximal-square/
+
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and
+return its area.
+
+Example:
+
+Input:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+Output: 4
+*/
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function(matrix) {
+
+};
+
+var test = [
+  [1,0,1,0,0],
+  [1,0,1,1,1],
+  [1,1,1,1,1],
+  [1,0,0,1,0]
+]
+
+console.log(maximalSquare(test) === 4);
