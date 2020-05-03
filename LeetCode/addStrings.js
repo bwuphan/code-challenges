06670279fe130d65884272da611615fff7fa894d
bwuphan <bwuphan@gmail.com>
2020-05-02 23:08:33 -0700

    Add solution for addStrings.js

--- LeetCode/addStrings.js
+++ LeetCode/addStrings.js
@@ -17,5 +17,34 @@ You must not use any built-in BigInteger library or convert the inputs to intege
  * @return {string}
  */
 var addStrings = function(num1, num2) {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
 
+  let sum = [];
+  let carry = 0;
+  while (i >= 0 || j >= 0) {
+    const n1 = i >= 0 ? num1[i] - '0' : 0;
+    const n2 = j >= 0 ? num2[j] - '0' : 0;
+
+    let curSum = n1 + n2 + carry;
+    if (curSum >= 10) {
+      carry = 1;
+      curSum = curSum % 10;
+    }
+    else {
+      carry = 0;
+    }
+    sum.push(curSum);
+    i--;
+    j--;
+  }
+
+  if (carry)
+    sum.push('1');
+
+
+  return sum.reverse().join('');
 };
+
+console.log(addStrings('999', '223'));
+console.log(11 % 10)
