750e512622ef9d43f52af3e4483b29a2a28ca009
bwuphan <bwuphan@gmail.com>
2020-05-03 22:58:58 -0700

    Work in progress minimumWindowSubstring.js

--- LeetCode/minimumWindowSubstring.js
+++ LeetCode/minimumWindowSubstring.js
@@ -25,20 +25,66 @@ If there is such window, you are guaranteed that there will always be only one u
 var minWindow = function(s, t) {
   let left, right = 0;
 
-  let solution = {};
+  const solutionDict = {};
+  const actionDict = {};
   for (let i = 0; i < t.length; ++i) {
-    const letter = t[i];
-    if (letter in solution) solution[letter]++;
-    else solution[letter] = 1;
-  }
-  console.log(solution);
+    const c = t[i];
+    if (!(c in actionDict))
+      actionDict[c] = 0;
 
-  for (let i = 0; i < s.length; ++i) {
-
+    if (!(c in solutionDict))
+      solutionDict[c] = 1;
+    else
+      solutionDict[c]++;
   }
+
+  let missingLetter = null;
+  let smallestStr = null;
+  for (left = 0, right = 0; right < s.length; ++right) {
+    const char = s[right];
+    if (char in actionDict)
+      actionDict[char]++;
+
+    if (char === missingLetter || checkSolution(actionDict, solutionDict)) {
+      missingLetter = null;
+      while (!missingLetter) {
+        const leftChar = s[left];
+        if (leftChar in solutionDict)
+          missingLetter = leftChar;
+        left++;
+      }
+      if (smallestStr === null || (right - left) < smallestStr.length)
+        smallestStr = s.slice(left - 1, right + 1)
+      actionDict[missingLetter]--;
+    }
+  }
+  left--;
+  actionDict[s[left]]++;
+  console.log(actionDict);
+  while (left < right) {
+    console.log(left);
+    const leftChar = s[left];
+    if (leftChar in actionDict)
+      actionDict[leftChar]--;
+    console.log(actionDict);
+    if (checkSolution(actionDict, solutionDict))
+      left++;
+    else
+      break;
+  }
+
+  return s.slice(left, right);
 };
 
-function check()
+function checkSolution(actionDict, solutionDict) {
+  for (let key in solutionDict) {
+    if (actionDict[key] < solutionDict[key])
+      return false;
+  }
+
+  return true;
+}
 
 
 console.log(minWindow(s = "ADOBECODEBANC", t = "ABC"));
+console.log(minWindow(s = "BBA", t = "AB"));
