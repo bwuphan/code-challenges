cf8a309b6b267e49fae89213379a6a47f3ad9d00
bwuphan <bwuphan@gmail.com>
2020-05-22 00:51:00 -0700

    Add working solution to LeetCode/combinationSum.js

--- LeetCode/combinationSum.js
+++ LeetCode/combinationSum.js
@@ -29,7 +29,46 @@ A solution set is:
   [3,5]
 ]
 */
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  if (!candidates || candidates.length === 0)
+    return [];
 
+  candidates.sort((a, b) => b - a);
+  const candidateSet = new Set(candidates);
+
+
+  const results = new Set();
+
+  const dfs = (sum, numString, maxNumIdx) => {
+    if (sum === target) {
+      results.add(numString.slice(1, numString.length));
+      return;
+    }
+    if (sum < 0)
+      return;
+
+    const diff = target - sum;
+
+    while (diff < candidates[maxNumIdx]) {
+      maxNumIdx++;
+    }
+    for (let i = maxNumIdx; i < candidates.length; ++i) {
+      const candidate = candidates[i];
+      const newNumString = `${numString},${candidate}`;
+      if (!candidateSet.has(newNumString))
+        dfs(sum + candidate, `${numString},${candidate}`, i);
+    }
+  }
+
+  dfs(0, '', 0);
+  return Array.from(results)
+    .map(sol => sol.split(','));
+};
 
 
 /**
@@ -38,5 +77,50 @@ A solution set is:
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
+  if (!candidates || candidates.length === 0)
+    return [];
 
+  candidates.sort((a, b) => b - a);
+  const candidateSet = new Set(candidates);
+
+
+  const results = new Set();
+
+  const dfs = (sum, numString, maxNumIdx) => {
+    if (sum === target) {
+      results.add(numString);
+      return;
+    }
+    if (sum < 0)
+      return;
+
+    const diff = target - sum;
+
+    if (candidateSet.has(diff)) {
+      const newNumString = numString.length > 0 ? `${numString},${diff}` : `${diff}`;
+      results.add(newNumString)
+    }
+    else {
+      while (diff < candidates[maxNumIdx]) {
+        maxNumIdx++;
+      }
+
+
+      for (let i = maxNumIdx; i < candidates.length; ++i) {
+        const candidate = candidates[i];
+        const newNumString = numString.length > 0 ? `${numString},${candidate}` : `${candidate}`;
+        if (!candidateSet.has(newNumString))
+          dfs(sum + candidate, newNumString, i);
+      }
+    }
+
+  }
+
+  dfs(0, '', 0);
+  return Array.from(results)
+    .map(sol => sol.split(','));
 };
+
+
+console.log(combinationSum(candidates = [2,3,6,7], target = 7));
+console.log(combinationSum(candidates = [2,3,5], target = 8));
