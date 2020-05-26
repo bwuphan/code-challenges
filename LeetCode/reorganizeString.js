6b1028ad0b563e5a89d2f8694a8c06a9bd3448bb
bwuphan <bwuphan@gmail.com>
2020-05-25 17:38:43 -0700

    Add LeetCode/reorganizeString.js

---
+++ LeetCode/reorganizeString.js
@@ -0,0 +1,180 @@
+/*
+https://leetcode.com/problems/reorganize-string/
+
+Given a string S, check if the letters can be rearranged so that two characters
+that are adjacent to each other are not the same.
+
+If possible, output any possible result.  If not possible, return the empty string.
+
+Example 1:
+
+Input: S = "aab"
+Output: "aba"
+Example 2:
+
+Input: S = "aaab"
+Output: ""
+Note:
+
+S will consist of lowercase letters and have length in range [1, 500].
+*/
+
+
+class MaxHeap {
+  constructor(array) {
+    this._heap = [];
+
+    if (array) {
+      array.forEach(num => this.insert(num));
+    }
+  }
+
+  insert(val) {
+    /*
+      Push the new val to the heap.
+      Set current idx to the last pushed index and get the parent index.
+      If there is no parent, we know we're in the right place and we can return.
+      Otherwise keep swapping until we find the right spot. The inserted val has to be greater
+      than its parent.
+     */
+    this._heap.push(val);
+
+    let thisIdx = this._heap.length - 1;
+    let parentIdx = this.getParentIdx(thisIdx);
+
+    if (parentIdx === null) return val;
+
+    while (parentIdx !== null && this._heap[thisIdx] > this._heap[parentIdx]) {
+      this.swap(parentIdx, thisIdx);
+      thisIdx = parentIdx;
+      parentIdx = this.getParentIdx(thisIdx);
+    }
+  }
+
+  swap(idx1, idx2) {
+    const temp = this._heap[idx1];
+    this._heap[idx1] = this._heap[idx2];
+    this._heap[idx2] = temp;
+  }
+
+  getParentIdx(idx) {
+    const pIdx = Math.floor((idx - 1) / 2);
+    return pIdx < 0 ? null : pIdx;
+  }
+
+  peek() {
+    const topOfHeap = this._heap.length ? this._heap[0] : null;
+    return topOfHeap;
+  }
+
+  getLeftChildIdx(idx) {
+    const lIdx = Math.floor((2 * idx) + 1);
+    return lIdx < this._heap.length ? lIdx : null;
+  }
+
+  getRightChildIdx(idx) {
+    const rIdx = Math.floor((2 * idx) + 2);
+    return rIdx < this._heap.length ? rIdx : null;
+  }
+
+  remove() {
+    /*
+      Swap the first element with the last and pop off the new last element.
+      Start at the first element and compare with the higher of its two children.
+      If the larger of the children is larger than the current element, swap the two positions.
+      Keep doing this until you find the right location.
+    */
+
+    if (this._heap.length === 0)
+      return null;
+
+    const lastIdx = this._heap.length - 1;
+    this.swap(0, lastIdx);
+
+    const returnVal = this._heap.pop();
+
+    let curIdx, leftChildIdx, rightChildIdx, largerIdx = null;
+
+    curIdx = 0;
+
+    while (true) {
+      leftChildIdx = this.getLeftChildIdx(curIdx);
+
+      rightChildIdx = this.getRightChildIdx(curIdx);
+
+      largerIdx = null;
+
+      if (rightChildIdx === null || this._heap[leftChildIdx] > this._heap[rightChildIdx]) {
+        largerIdx = leftChildIdx;
+      }
+      else {
+        largerIdx = rightChildIdx;
+      }
+
+      if (this._heap[curIdx] < this._heap[largerIdx]) {
+        this.swap(curIdx, largerIdx);
+        curIdx = largerIdx;
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+    return returnVal;
+  }
+
+  log() {
+    console.log(this._heap);
+  }
+}
+
+
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reorganizeString = function(S) {
+  const occurObj = {};
+  let numLetters = 0;
+  for (let i = 0; i < S.length; ++i) {
+    const letter = S[i];
+    if (letter in occurObj)
+      occurObj[letter]++;
+    else {
+      occurObj[letter] = 1;
+      numLetters++;
+    }
+  }
+
+  const occurArr = [];
+  for (let letter in occurObj) {
+    occurArr.push({ letter, occurences: occurObj[letter] });
+  }
+
+  occurArr.sort((a, b) => b.occurences - a.occurences);
+
+  if (occurArr[0].occurences > ((S.length + 1) / 2))
+    return '';
+
+  let returnStr = '';
+  while (numLetters > 0) {
+    for (let i = 0; i < occurArr.length; ++i) {
+      const item = occurArr[i];
+      if (item.occurences) {
+        returnStr += item.letter;
+        item.occurences--;
+      }
+
+      if (item.occurences <= 0) {
+        numLetters--;
+        occurArr.splice(i, 1);
+      }
+    }
+  }
+
+  return returnStr;
+};
+
+console.log(reorganizeString('aaabccc'));
+console.log(reorganizeString('aaab'));
