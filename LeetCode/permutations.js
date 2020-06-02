855c2ea6430db8a3647bfd845360416655010e5a
bwuphan <bwuphan@gmail.com>
2020-06-01 22:46:44 -0700

    Add backtracking to LeetCode/permutations.js

--- LeetCode/permutations.js
+++ LeetCode/permutations.js
@@ -27,33 +27,52 @@ var permute = function(nums) {
 
   let permutations = [];
 
-  const dfs = function(startArr, resultArr) {
-    // If there is nothing left in the startArr, we know we have a result.
-    if (startArr.length === 0) {
-      permutations.push(resultArr);
+  const visited = new Set();
+  const dfs = (numArr) => {
+    if (visited.size === nums.length) {
+      permutations.push(numArr);
+      return;
     }
 
-    // Loop through the startArr.
-    for (let i = 0; i < startArr.length; ++i) {\
-      // Make copies to avoid side effects.
-      let resultArrCpy = [...resultArr];
-      let startArrCpy = [...startArr];
+    nums.forEach(n => {
+      if (!visited.has(n)) {
+        numArrCopy = [...numArr];
+        numArrCopy.push(n);
+        visited.add(n);
+        dfs(numArrCopy);
+        visited.delete(n);
+      }
+    });
 
-      // Push the next element onto the result array copy.
-      resultArrCpy.push(startArr[i]);
-
-      // Take the element out of the start array copy.
-      startArrCpy.splice(i, 1);
-
-      // Recurse.
-      dfs(startArrCpy, resultArrCpy);
-    }
   }
+  dfs('');
+  // const dfs = function(startArr, resultArr) {
+  //   // If there is nothing left in the startArr, we know we have a result.
+  //   if (startArr.length === 0) {
+  //     permutations.push(resultArr);
+  //   }
 
-  dfs(nums, []);
+  //   // Loop through the startArr.
+  //   for (let i = 0; i < startArr.length; ++i) {
+  //     // Make copies to avoid side effects.
+  //     let resultArrCpy = [...resultArr];
+  //     let startArrCpy = [...startArr];
+
+  //     // Push the next element onto the result array copy.
+  //     resultArrCpy.push(startArr[i]);
+
+  //     // Take the element out of the start array copy.
+  //     startArrCpy.splice(i, 1);
+
+  //     // Recurse.
+  //     dfs(startArrCpy, resultArrCpy);
+  //   }
+  // }
+
+  // dfs(nums, []);
   return permutations;
 };
 
 
 console.log(permute([1,2,3]));
-console.log(permute([1]));
+// console.log(permute([1]));
