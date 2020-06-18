5b24777e4067cf4b3d789b411ff36cfdba442cf3
bwuphan <bwuphan@gmail.com>
2020-06-17 22:57:52 -0700

    Add solution for LeetCode/pacificAtlanticWaterFlow.js

--- LeetCode/pacificAtlanticWaterFlow.js
+++ LeetCode/pacificAtlanticWaterFlow.js
@@ -39,5 +39,65 @@ Return:
  * @return {number[][]}
  */
 var pacificAtlantic = function(matrix) {
+  const results = [];
+  let done, p, a, visited;
+  const dfs = (row, col) => {
+    if (done || row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || visited[row][col])
+      return false;
 
+    visited[row][col] = true;
+
+    if ((row === 0 || col === 0) && !p) {
+      p = true;
+    }
+
+    if ((row === matrix.length - 1 || col === matrix[0].length - 1) && !a) {
+      a = true;
+    }
+
+    if (a && p) {
+      done = true;
+      return true;
+    }
+
+    const cur = matrix[row][col];
+
+    if ((row + 1) < matrix.length && cur >= matrix[row + 1][col])
+      dfs(row + 1, col);
+
+    if ((row - 1) >= 0 && cur >= matrix[row - 1][col])
+      dfs(row - 1, col);
+
+    if ((col + 1) < matrix[0].length && cur >= matrix[row][col + 1])
+      dfs(row, col + 1);
+
+    if ((col - 1) >= 0 && cur >= matrix[row][col - 1])
+      dfs(row, col - 1);
+
+    visited[row][col] = false;
+  };
+
+  for (let i = 0; i < matrix.length; ++i) {
+    for (let j = 0; j < matrix[0].length; ++j) {
+      visited = matrix.map(row => new Array(matrix[0].length));
+      done = false;
+      a = false;
+      p = false;
+      dfs(i, j);
+
+      if (done && a && p)
+        results.push([i, j]);
+    }
+  }
+
+  return results;
 };
+
+console.log(pacificAtlantic([
+  [1,2,2,3,5],
+  [3,2,3,4,4],
+  [2,4,5,3,1],
+  [6,7,1,4,5],
+  [5,1,1,2,4]
+]));
+
