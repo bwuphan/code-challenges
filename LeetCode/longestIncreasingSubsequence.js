4411b31f29c1124be3dc2892e68cabddaa9cfdb3
bwuphan <bwuphan@gmail.com>
2020-06-26 15:44:52 -0700

    Add solution to longestIncreasingSubsequence.js

--- LeetCode/longestIncreasingSubsequence.js
+++ LeetCode/longestIncreasingSubsequence.js
@@ -21,5 +21,39 @@ Follow up: Could you improve it to O(n log n) time complexity?
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
+  const dp = new Array(nums.length);
+  dp[0] = 1;
 
+  let max = 0;
+  for (let i = 1; i < nums.length; ++i) {
+    let curMax = 1;
+    for (let j = 0; j < i; ++j) {
+      if (nums[i] > nums[j] && dp[j] >= curMax) {
+        curMax = dp[j] + 1;
+      }
+    }
+    dp[i] = curMax;
+
+    if (max < curMax)
+      max = curMax;
+  }
+
+  return nums.length ? dp[nums.length - 1] : 0;
 };
+
+
+/*
+Solution:
+Create a dp array that keeps track of the max subsequence up to the index.
+
+Create a nested loop that goes up to the ith index comparing the two numbers.
+
+If we find that nums[i] > nums[j], we know that the longest increasing subsequence at that number
+would be dp[j] + 1.
+
+Return the longest max we could find.
+*/
+
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18]) === 4);
+
+console.log(lengthOfLIS([1,3,6,7,9,4,10,5,6]));
