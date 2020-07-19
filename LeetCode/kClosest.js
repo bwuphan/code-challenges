ff11ba21a834050a143dcf970f0d10f609da9eea
bwuphan <bwuphan@gmail.com>
2020-07-18 18:04:04 -0700

    Add solution with heap

--- LeetCode/kClosest.js
+++ LeetCode/kClosest.js
@@ -1,3 +1,147 @@
+/*
+https://leetcode.com/problems/k-closest-points-to-origin/
+
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique
+(except for the order that it is in.)
+
+
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+
+
+Note:
+
+1 <= K <= points.length <= 10000
+-10000 < points[i][0] < 10000
+-10000 < points[i][1] < 10000
+*/
+
+class MinHeap {
+  constructor(array) {
+    this._heap = [];
+
+    if (array) {
+      array.forEach(num => this.insert(num));
+    }
+  }
+
+  insert(pointObj) {
+    /*
+      Push the new val to the heap.
+      Set current idx to the last pushed index and get the parent index.
+      If there is no parent, we know we're in the right place and we can return.
+      Otherwise keep swapping until we find the right spot. The inserted val has to be greater
+      than its parent.
+     */
+    this._heap.push(pointObj);
+
+    let thisIdx = this._heap.length - 1;
+    let parentIdx = this.getParentIdx(thisIdx);
+
+    if (parentIdx === null) return pointObj;
+
+    while (parentIdx !== null && this._heap[thisIdx].dist < this._heap[parentIdx].dist) {
+      this.swap(parentIdx, thisIdx);
+      thisIdx = parentIdx;
+      parentIdx = this.getParentIdx(thisIdx);
+    }
+  }
+
+  swap(idx1, idx2) {
+    const temp = this._heap[idx1];
+    this._heap[idx1] = this._heap[idx2];
+    this._heap[idx2] = temp;
+  }
+
+  getParentIdx(idx) {
+    const pIdx = Math.floor((idx - 1) / 2);
+    return pIdx < 0 ? null : pIdx;
+  }
+
+  peek() {
+    const topOfHeap = this._heap.length ? this._heap[0] : null;
+    console.log('Top:', topOfHeap);
+    return topOfHeap;
+  }
+
+  getLeftChildIdx(idx) {
+    const lIdx = Math.floor((2 * idx) + 1);
+    return lIdx < this._heap.length ? lIdx : null;
+  }
+
+  getRightChildIdx(idx) {
+    const rIdx = Math.floor((2 * idx) + 2);
+    return rIdx < this._heap.length ? rIdx : null;
+  }
+
+  remove() {
+    /*
+      Swap the first element with the last and pop off the new last element.
+      Start at the first element and compare with the lower of its two children.
+      If the smaller of the children is smaller than the current element, swap the two positions.
+      Keep doing this until you find the right location.
+    */
+
+    if (this._heap.length === 0)
+      return null;
+
+    const lastIdx = this._heap.length - 1;
+    this.swap(0, lastIdx);
+
+    const returnVal = this._heap.pop();
+
+    let curIdx, leftChildIdx, rightChildIdx, smallerIdx = null;
+
+    curIdx = 0;
+
+    while (true) {
+      leftChildIdx = this.getLeftChildIdx(curIdx);
+
+      rightChildIdx = this.getRightChildIdx(curIdx);
+
+      smallerIdx = null;
+
+      if (rightChildIdx === null || this._heap[leftChildIdx].dist < this._heap[rightChildIdx].dist) {
+        smallerIdx = leftChildIdx;
+      }
+      else {
+        smallerIdx = rightChildIdx;
+      }
+
+      if (this._heap[smallerIdx] && this._heap[curIdx] && this._heap[curIdx].dist > this._heap[smallerIdx].dist) {
+        this.swap(curIdx, smallerIdx);
+        curIdx = smallerIdx;
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+    return returnVal;
+  }
+
+  log() {
+    console.log(this._heap);
+  }
+}
+
 /**
  * @param {number[][]} points
  * @param {number} K
@@ -6,14 +150,19 @@
 var kClosest = function(points, K) {
   var calcDist = (point) => Math.sqrt(point[0]*point[0] + point[1]*point[1]);
 
+  const heap = new MinHeap();
   let distArr = points.map(point => {
-    return {
+    heap.insert({
       point: point,
-      distance : calcDist(point)
-    };
+      dist : calcDist(point)
+    });
   });
 
-  return distArr.sort((a, b) => a.distance - b.distance).slice(0, K).map(pointObj => pointObj.point);
+  const results = [];
+  for (let i = 0; i < K; ++i)
+    results.push(heap.remove().point);
+
+  return results;
 };
 
 console.log(kClosest([[1,3],[-2,2]], 1))
