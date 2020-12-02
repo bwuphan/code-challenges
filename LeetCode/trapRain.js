d1212b2734bc75c2151a0e1c179fbe04f068fa80
bwuphan <bwuphan@gmail.com>
2020-12-01 21:06:59 -0800

    Add comments

--- LeetCode/trapRain.js
+++ LeetCode/trapRain.js
@@ -7,22 +7,25 @@
 
 const trap = function(height) {
 
+  // Create two max objects from left and right and init both starts to 0.
   let maxLeft = 0, maxRight = 0;
   let maxLeftObj = { '0' : 0 };
   let maxRightObj = {}; maxRightObj[height.length - 1] = 0;
 
+  // Fill in max left object going from left to right.
   for (let i = 1; i < height.length; ++i) {
     if (height[i - 1] > maxLeft) maxLeft = height[i - 1];
     maxLeftObj[i] = maxLeft;
   }
 
+  // Fill in max right object going from right to left.
   for (let i = height.length - 2; i >= 0; --i) {
     if (height[i + 1] > maxRight) maxRight = height[i + 1];
     maxRightObj[i] = maxRight;
   }
 
-
   let heightSum = 0;
+  // Loop through and get each height unit is the min between max left and right at the same spot.
   for (let i = 0; i < height.length; ++i) {
     const hCol = Math.min(maxLeftObj[i], maxRightObj[i]) - height[i];
 
@@ -32,6 +35,9 @@ const trap = function(height) {
   return heightSum;
 };
 
+
+const height = [0,1,0,2,1,0,1,3,2,1,2,1]
+console.log(trap(height));
 
 // Brute Force
 
