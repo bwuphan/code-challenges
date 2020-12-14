10c7c474e646f4e25075c97aa3a1831fd23eccd0
bwuphan <bwuphan@gmail.com>
2020-12-13 17:36:26 -0800

    Add closestBinarySearchTreeValue

---
+++ LeetCode/closestBinarySearchTreeValue.js
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/closest-binary-search-tree-value/
+
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest
+to the target.
+
+Note:
+
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+Example:
+
+Input: root = [4,2,5,1,3], target = 3.714286
+
+    4
+   / \
+  2   5
+ / \
+1   3
+
+Output: 4
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, target) {
+
+};
