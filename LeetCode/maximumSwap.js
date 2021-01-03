3fb57cebf2b5dd07a4bcd76116d71652a60233e5
bwuphan <bwuphan@gmail.com>
2021-01-02 18:14:28 -0800

    Add solution

--- LeetCode/maximumSwap.js
+++ LeetCode/maximumSwap.js
@@ -22,5 +22,38 @@ The given number is in the range [0, 108]
  * @return {number}
  */
 var maximumSwap = function(num) {
+  const maxFromRight = new Map();
+  const numArray = num.toString().split('').map(n => +n);
 
+  let maxObj = null;
+  for (let i = numArray.length - 1; i >= 0; --i) {
+    const curNum = numArray[i];
+    if (maxObj === null || curNum > maxObj.max) {
+      maxObj = { max: curNum, idx: i };
+      maxFromRight.set(i, maxObj);
+    }
+    else maxFromRight.set(i, maxObj)
+  }
+
+  for (let i = 0; i < numArray.length; ++i) {
+    const curNum = numArray[i];
+    const curMax = maxFromRight.get(i);
+
+    if (curNum < curMax.curMax) {
+      const temp = numArray[curMax.idx];
+      numArray[curMax.idx] = curNum;
+      numArray[i] = temp;
+
+      return numArray.join('');
+    }
+  }
+
+  return num;
 };
+
+
+console.log(maximumSwap(2736))
+console.log(maximumSwap(9973))
+
+console.log(maximumSwap(887873))
+console.log(maximumSwap(1993))
