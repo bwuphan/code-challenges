4bdddd0e8d5637706c6a5c7f41559979f74925ea
bwuphan <bwuphan@gmail.com>
2021-01-03 16:09:49 -0800

    Add rangeSumOfBst

---
+++ LeetCode/rangeSumOfBst.js
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/range-sum-of-bst/
+
+Given the root node of a binary search tree, return the sum of values of all nodes with a value in
+the range [low, high].
+
+
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 2 * 104].
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+All Node.val are unique.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+
+};
