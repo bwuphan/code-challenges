14c4e118e01680db77c96a529b564b8f5c4f42df
bwuphan <bwuphan@gmail.com>
2021-01-06 22:21:37 -0800

    Add more intuitive but slower solution

--- LeetCode/subarraySumEqualsK.js
+++ LeetCode/subarraySumEqualsK.js
@@ -41,11 +41,26 @@ var subarraySum = function(nums, k) {
 
     if (sum in sumMap) sumMap[sum]++;
     else sumMap[sum] = 1;
-    console.log(sumMap)
+
   });
 
   return count;
 };
 
+var subarraySum = function(nums, k) {
+  let count = 0;
+
+  for (let start = 0; start < nums.length; ++start) {
+    let sum = 0;
+    for (let end = start; end < nums.length; ++end) {
+      sum += nums[end];
+      if (sum === k) count++;
+    }
+  }
+
+  return count;
+}
+
 console.log(subarraySum([1,1,1], 2));
 console.log(subarraySum([1,2,3], 3))
+console.log(subarraySum([3,4,7,2,-3,1,4,2], 7))
