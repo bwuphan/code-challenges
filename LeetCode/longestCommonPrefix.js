9ec6660847c689320b34fb03f71e9d6738312e67
bwuphan <bwuphan@gmail.com>
2021-01-17 15:44:10 -0800

    Add solution horizontal scan

--- LeetCode/longestCommonPrefix.js
+++ LeetCode/longestCommonPrefix.js
@@ -30,7 +30,39 @@ strs[i] consists of only lower-case English letters.
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+  if (!strs || !strs.length)
+    return '';
+  if (strs.length === 1)
+    return strs[0];
 
+  let longest = '';
+
+  for (let i = 0; i < Math.min(strs[0].length, strs[1].length); ++i) {
+    if (strs[0][i] === strs[1][i])
+      longest += strs[0][i];
+    else
+      break;
+  }
+
+  if (!longest)
+    return '';
+
+  if (strs.length === 2)
+    return longest;
+
+  for (let i = 2; i < strs.length; ++i) {
+    const str = strs[i];
+
+    let newLongest = '';
+    for (let j = 0; j < Math.min(str.length, longest.length); ++j) {
+      if (str[j] === longest[j])
+        newLongest += str[j]
+    }
+
+    longest = newLongest;
+  }
+
+  return longest
 };
 
 
