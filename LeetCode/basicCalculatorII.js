f080f5e05038e8685d317cb4f98384e00afdc0f3
bwuphan <bwuphan@gmail.com>
2021-01-17 16:22:45 -0800

    Add basicCalculatorII

---
+++ LeetCode/basicCalculatorII.js
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/basic-calculator-ii/
+
+Given a string s which represents an expression, evaluate this expression and return its value.
+
+The integer division should truncate toward zero.
+
+
+
+Example 1:
+
+Input: s = "3+2*2"
+Output: 7
+Example 2:
+
+Input: s = " 3/2 "
+Output: 1
+Example 3:
+
+Input: s = " 3+5 / 2 "
+Output: 5
+
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+s represents a valid expression.
+All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+The answer is guaranteed to fit in a 32-bit integer.
+*/
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+
+};
+
+
+console.log(calculate("3+2*2"))
+console.log(calculate(" 3/2 "))
+console.log(calculate(" 3+5 / 2 "))
