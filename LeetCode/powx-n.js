d4d9619c126fe9d42a7aa0c309fbf9435f5db9fd
bwuphan <bwuphan@gmail.com>
2021-02-22 21:54:32 -0800

    Add comments

--- LeetCode/powx-n.js
+++ LeetCode/powx-n.js
@@ -33,24 +33,48 @@ Constraints:
  * @return {number}
  */
 var myPow = function(x, n) {
+  // If less than 0, rearange to 1 / x.
   if (n < 0) {
     x = 1 / x;
     n = -n;
   }
 
-  console.log(x, n);
-
   let result = 1;
   while (n > 0) {
-    if (n % 2 === 1)
-      result *= x;
-    x *= x;
-    n = Math.floor(n / 2);
+    if (n % 2 === 1) result *= x; // If odd, multiply it by itself once so it's even again.
+    x *= x; // Basically squaring it here.
+    n = Math.floor(n / 2); // Need math.floor so it's a round number.
   }
 
   return result;
 };
 
+/*
+Solution:
+
+Works because x^n = (x^(n /2))^2
+We are going to iteratively do this
+
+Example: 3^11
+first iteration:
+odd, so result = 3
+x = 9
+n = 5
+
+2:
+odd so multiple result by x. 3 * 9 = 27
+x = 81
+n = 2
+
+3:
+x = 81 * 81 = 6561
+n = 1
+
+4:
+odd so multiply result by x. 27 * 6561
+Doesnt matter anymore because we have our result.
+*/
 
 console.log(myPow(2.00000, 10));
 // console.log(myPow(2.00000, -2));
+console.log(myPow(3, 11))
