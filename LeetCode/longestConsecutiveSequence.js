7df9d36ff638ee0a2859d1bc814a939652abd81b
bwuphan <bwuphan@gmail.com>
2021-02-25 22:59:51 -0800

    Add solution

--- LeetCode/longestConsecutiveSequence.js
+++ LeetCode/longestConsecutiveSequence.js
@@ -31,5 +31,37 @@ Follow up: Could you implement the O(n) solution?
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+  const numSet = new Set(nums);
 
+  let longest = 0;
+
+  numSet.forEach(num => {
+    // Only start the inner loop if the num is the smallest of its sequence.
+    if (!numSet.has(num - 1)) {
+      // Get the longest sequence at this num.
+      let curLongest = 1;
+      while (numSet.has(++num)) {
+        curLongest++;
+      }
+
+      if (curLongest > longest) {
+        longest = curLongest;
+      }
+    }
+  });
+
+  return longest;
 };
+
+/*
+Loop through nums set until you find the smallest in the sequence.
+From there, get the longest current sequence.
+Compare to longest sequence.
+
+Time complexity is O(N)
+Space O(N)
+
+*/
+
+console.log(longestConsecutive([100,4,200,1,3,2]))
+console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
