43ea1366d252c1184b98222e6117343645836870
bwuphan <bwuphan@gmail.com>
2021-02-27 16:48:21 -0800

    Add left and right to remove

--- LeetCode/removeInvalidParentheses.js
+++ LeetCode/removeInvalidParentheses.js
@@ -26,7 +26,29 @@ Output: [""]
  * @return {string[]}
  */
 var removeInvalidParentheses = function(s) {
+  let closeRemovalNum = 0;
+  let unclosedOpens = 0;
+  for (let i = 0; i < s.length; ++i) {
+    const char = s[i];
+    if (char === '(')
+      unclosedOpens++;
+    else if (char === ')') {
+      if (unclosedOpens > 0) unclosedOpens--;
+      else closeRemovalNum++;
+    }
+  }
 
+  let openRemoveNum = 0;
+  let unopenCloses = 0;
+  for (let i = s.length - 1; i >= 0; --i) {
+    const char = s[i];
+    if (char === ')')
+      unopenCloses++;
+    else if (char === '(') {
+      if (unopenCloses) unopenCloses--;
+      else openRemoveNum++;
+    }
+  }
+  console.log(closeRemovalNum, openRemoveNum)
 };
-
 console.log(removeInvalidParentheses("()())()"))
