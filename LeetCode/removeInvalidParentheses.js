440d5061bee6589d1d1d63469070c087f4cba707
bwuphan <bwuphan@gmail.com>
2021-02-27 17:40:10 -0800

    Add comments

--- LeetCode/removeInvalidParentheses.js
+++ LeetCode/removeInvalidParentheses.js
@@ -60,6 +60,7 @@ var removeInvalidParentheses = function(s) {
     if (i >= s.length) {
       if (leftCount === rightCount && !solutions.has(string))
         solutions.add(string);
+
       return;
     }
 
@@ -79,6 +80,7 @@ var removeInvalidParentheses = function(s) {
         recurse(string, i, leftCount, rightCount, leftRemovalCount - 1, rightRemovalCount);
       }
 
+      // Have to do unconditionally since we don't know immediately if this is valid or not.
       recurse(string + '(', i, leftCount + 1, rightCount, leftRemovalCount, rightRemovalCount);
     }
     else recurse(string + char, i, leftCount, rightCount, leftRemovalCount, rightRemovalCount);
@@ -91,3 +93,29 @@ var removeInvalidParentheses = function(s) {
 // console.log(removeInvalidParentheses('(a)())()'));
 // console.log(removeInvalidParentheses(')('))
 console.log(removeInvalidParentheses(')()('))
+
+
+/*
+Solution:
+1. Get number of possible removals for left and right. This is always going to
+be constant for all solutions because the problem is trying to make the minimum number of
+removals.
+
+2. We are going to recurse through and use backtracking to figure out all possible solutions.
+We keep track of the removal counts because if we are out of removes, we know not to create
+a new recursion case where we remove ( or )
+
+3. Base case is if we are at the end. Add to solution if the left and right counts are even.
+
+4. If we encounter a ), we do two things:
+If there is still room to remove, we recurse with a removal move.
+If we have a greater number of ( vs ), we know this is a valid paren so far so we recurse and add ).
+
+5. If we encounter a (, we do two things:
+If we have room to remove (leftRemovalCount), we recurse with a removal move.
+Since for (, we can't tell immediately if this is valid by checking the number of ), the only thing
+we can do is recurse every time and add the left paren.
+
+6. If normal character, recurse and add char to string.
+
+*/
