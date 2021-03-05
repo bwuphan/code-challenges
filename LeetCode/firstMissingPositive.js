b49865f319b39767dab0c6890867eba7a35b5f6c
bwuphan <bwuphan@gmail.com>
2021-03-04 21:12:32 -0800

    Add constant space solution

--- LeetCode/firstMissingPositive.js
+++ LeetCode/firstMissingPositive.js
@@ -43,5 +43,36 @@ var firstMissingPositive = function(nums) {
   return nums.length + 1;
 };
 
+
+// Constant space
+firstMissingPositive = (nums) => {
+  if (nums.indexOf(1) < 0) return 1;
+
+  nums.forEach((num, i) => {
+    if (num > nums.length || num < 1)
+      nums[i] = 1;
+  });
+
+  let result = null;
+  nums.forEach((num, i) => {
+    const a = Math.abs(nums[i])
+    if (a === nums.length)
+      nums[0] = -Math.abs(nums[0]);
+    else
+      nums[a] = -Math.abs(nums[a])
+  })
+
+  for (let i = 1; i < nums.length; ++i) {
+    if (nums[i] > 0)
+      return i
+  }
+
+  return nums[0] > 0 ? nums.length : nums.length + 1;
+}
+
+// console.log(firstMissingPositive([]))
 console.log(firstMissingPositive([1,2,0]));
-console.log(firstMissingPositive([3,4,-1,1]))
+// console.log(firstMissingPositive([3,4,-1,1]))
+// console.log(firstMissingPositive([3,4,-1,-2,1,5,16,0,2,0]))
+
+
