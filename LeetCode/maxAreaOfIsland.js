cd8c8abec85cf30e3c1a87a208b437a5e3937c36
bwuphan <bwuphan@gmail.com>
2021-03-04 20:08:48 -0800

    Add LeetCode/maxAreaOfIsland.js

---
+++ LeetCode/maxAreaOfIsland.js
@@ -0,0 +1,76 @@
+/*
+https://leetcode.com/problems/max-area-of-island/
+
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land)
+connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid
+are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum
+area is 0.)
+
+Example 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected
+4-directionally.
+
+Example 2:
+
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+*/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+  let max = 0;
+  for (let row = 0; row < grid.length; ++row) {
+    for (let col = 0; col < grid[0].length; ++col) {
+      const cell = grid[row][col];
+
+      if (cell === 1) {
+        const curMax = getArea(row, col);
+        if (curMax > max)
+          max = curMax;
+      }
+    }
+  }
+
+  function getArea(row, col) {
+    if (!grid[row] || !grid[row][col] || grid[row][col] !== 1) {
+      return 0;
+    }
+    else {
+      let sum = 1;
+      grid[row][col] = -1;
+      sum += getArea(row + 1, col);
+      sum += getArea(row - 1, col);
+      sum += getArea(row, col + 1);
+      sum += getArea(row, col - 1);
+      return sum;
+    }
+  }
+
+  return max;
+};
+
+
+console.log(maxAreaOfIsland(
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]))
