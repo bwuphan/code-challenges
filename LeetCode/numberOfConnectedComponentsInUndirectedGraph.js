ac401109d3ee63a1e7642d6a6bf58ba41c1d1757
bwuphan <bwuphan@gmail.com>
2021-03-27 13:19:59 -0700

    Add LeetCode/numberOfConnectedComponentsInUndirectedGraph.js

---
+++ LeetCode/numberOfConnectedComponentsInUndirectedGraph.js
@@ -0,0 +1,42 @@
+/*
+https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+
+You have a graph of n nodes. You are given an integer n and an array edges where
+edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
+
+
+
+Example 1:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+Example 2:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+
+
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
+*/
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var countComponents = function(n, edges) {
+
+};
+
+console.log(countComponents(5, [[0,1],[1,2],[3,4]]))
