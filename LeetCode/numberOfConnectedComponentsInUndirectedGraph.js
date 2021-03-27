fb4a950a04a88353e6973e7617b7b2b077b9e931
bwuphan <bwuphan@gmail.com>
2021-03-27 13:49:24 -0700

    Add solution

--- LeetCode/numberOfConnectedComponentsInUndirectedGraph.js
+++ LeetCode/numberOfConnectedComponentsInUndirectedGraph.js
@@ -36,7 +36,43 @@ There are no repeated edges.
  * @return {number}
  */
 var countComponents = function(n, edges) {
+  const graph = new Array(n);
 
+  edges.forEach(edge => {
+    if (!graph[edge[0]]) graph[edge[0]] = [edge[1]];
+    else graph[edge[0]].push(edge[1]);
+
+    if (!graph[edge[1]]) graph[edge[1]] = [edge[0]];
+    else graph[edge[1]].push(edge[0]);
+  });
+
+  const visited = new Set();
+  function dfs(idx) {
+    visited.add(idx);
+    if (graph[idx]) {
+      graph[idx].forEach(i => {
+        if (!visited.has(i))
+          dfs(i);
+      });
+    }
+  };
+
+  let numComponents = 0;
+  for (let i = 0; i < n; ++i) {
+    if (!visited.has(i)) {
+      dfs(i);
+      numComponents++;
+    }
+  }
+
+  return numComponents;
 };
 
+/*
+Solution:
+
+DFS through and get components
+*/
+
 console.log(countComponents(5, [[0,1],[1,2],[3,4]]))
+console.log(countComponents(n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]))
