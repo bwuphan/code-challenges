b7f055c3ce494867790736d7b0cd751ce5c3e20a
bwuphan <bwuphan@gmail.com>
2021-04-04 10:28:27 -0700

    Add solution

--- LeetCode/findPeakElement.js
+++ LeetCode/findPeakElement.js
@@ -38,5 +38,24 @@ Follow up: Could you implement a solution with logarithmic complexity?
  * @return {number}
  */
 var findPeakElement = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[mid + 1])
+      right = mid;
+    else
+      left = mid + 1;
+  }
+
+  return left;
 };
+
+/*
+Solution:
+*/
+// console.log(findPeakElement([1,2,3,1]))
+console.log(findPeakElement([1,2,1,3,5,6,4]))
+// console.log(findPeakElement([1, 2]))
