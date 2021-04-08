49ec2ad0c02ff24ca2ae249c63df7d16d408d622
bwuphan <bwuphan@gmail.com>
2021-04-08 00:01:11 -0700

    Add checkInclusion

---
+++ LeetCode/checkInclusion.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+
+};
+
+
+// console.log(checkInclusion(s1 = "ab", s2 = "eidbaooo"))
+// console.log(checkInclusion(s1= "ab", s2 = "eidboaoo"))
+console.log(checkInclusion("adc","dcda"))
