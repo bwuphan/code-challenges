3203d6cb374d065ec3a5783b390b419c06104f23
bwuphan <bwuphan@gmail.com>
2021-04-10 23:02:04 -0700

    Add more efficient solution

--- LeetCode/maxSubarray.js
+++ LeetCode/maxSubarray.js
@@ -20,31 +20,22 @@ If you have figured out the O(n) solution, try coding another solution using the
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-  // Make a copy of the array so we don't have side effects.
-  nums = [...nums];
+//
 
-  // Set max to the first element of nums because first element's max is itself.
-  let max = nums[0];
 
-  // Start array at 1 since we already looked at 0 element.
+const maxSubArray = (nums) => {
+  let curMaxSum = nums[0];
+  let maxSum = nums[0];
+
   for (let i = 1; i < nums.length; ++i) {
-    const prevNum = nums[i - 1];
+    const num = nums[i];
+    curMaxSum = Math.max(num, curMaxSum + num)
 
-    // If prevNum is greater than 0, that means adding it to the current num is larger than
-    // current num by itself. If that's the case, we set nums[i] to itself plus the previous num.
-    if (prevNum > 0) {
-      nums[i] = prevNum + nums[i];
-    }
-
-    // If nums[i] is greater than the max, we have a new max.
-    if (nums[i] > max) {
-      max = nums[i];
-    }
+    if (curMaxSum > maxSum)
+      maxSum = curMaxSum;
   }
 
-  return max;
-};
-
+  return maxSum;
+}
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
